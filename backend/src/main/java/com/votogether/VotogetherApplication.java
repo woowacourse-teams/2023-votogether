d5452511596af87445ef2895809d3b5fdab90231
woo-chang <clllickme@naver.com>
2023-07-04 14:57:16 +0900

    feat: (#4) 스프링 프로젝트 생성

---
+++ backend/src/main/java/com/votogether/VotogetherApplication.java
@@ -0,0 +1,13 @@
+package com.votogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotogetherApplication.class, args);
+	}
+
+}

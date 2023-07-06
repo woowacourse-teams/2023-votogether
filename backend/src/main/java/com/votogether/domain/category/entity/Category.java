0a7f14ae4c02ed00b4200140a348ae8e7d25aa6e
JunH <tjdtls690@gmail.com>
2023-07-06 15:16:28 +0900

    feat: (#8) 카테고리 Entity, Repository 작성

---
+++ backend/src/main/java/com/votogether/domain/category/entity/Category.java
@@ -0,0 +1,21 @@
+package com.votogether.domain.category.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+}

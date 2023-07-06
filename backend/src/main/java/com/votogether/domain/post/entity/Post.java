02c8930776581b8ef9986dc56cc208c2fea9a50d
JunH <tjdtls690@gmail.com>
2023-07-06 15:17:49 +0900

    feat: (#8) 게시글 Entity, Repository 작성

---
+++ backend/src/main/java/com/votogether/domain/post/entity/Post.java
@@ -0,0 +1,30 @@
+package com.votogether.domain.post.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import java.time.LocalDateTime;
+import lombok.Getter;
+
+@Getter
+@Entity
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long memberId;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, length = 1000)
+    private String content;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+}

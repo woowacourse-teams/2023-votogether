1c2bec734ea90627f0fedfe313925aff434db94c
최우창 <clllickme@naver.com>
2023-07-11 21:48:29 +0900

    Jpa Auditing, Swagger 설정 (#27)
    * feat: (#26) JPA Auditing 설정 추가
    
    * feat: (#26) Swagger 설정 추가
    
    * feat: (#26) 헬스 체크 기능 구현
    
    * chore: (#26) 코드 정렬
    
    * refactor: (#26) 스프링 프로퍼티 불변으로 수정

---
+++ backend/src/main/java/com/votogether/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.votogether.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}

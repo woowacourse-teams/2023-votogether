a034b7beabf65021e244747d4e9ecba5759b8989
chsua <csj1919@naver.com>
2023-07-12 11:23:17 +0900

    design: (#15) 네모버튼 outline -> border로 수정

--- frontend/src/components/common/SquareButton/style.ts
+++ frontend/src/components/common/SquareButton/style.ts
@@ -9,7 +9,7 @@ export const Button = styled.button<ButtonProps>`
 
   width: 100%;
   height: 100%;
-  outline: 2px solid #ff7877;
+  border: 2px solid #ff7877;
   border-radius: 5px;
 
   background-color: ${props => (props.theme === 'blank' ? 'rgba(0,0,0,0)' : '#ff7877')};

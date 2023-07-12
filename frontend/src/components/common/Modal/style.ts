f738b28ba59fb0173b302a9b53eacdcca634cadb
jero_kang <81199414+inyeong-kang@users.noreply.github.com>
2023-07-12 14:25:15 +0900

    공통 모달 컴포넌트 구현 (#28)
    * refactor: (#17) App.tsx에서 Example 컴포넌트 import
    Button 컴포넌트 대신 Example 컴포넌트 import
    
    * feat: (#17) Modal 컴포넌트 구현 및 스토리 작성
    Modal 컴포넌트는 3가지 상태 - Default, Wide(디바이스 width가 넓은 경우), With Close Button(모달 안에 취소 버튼이 있는 경우)로 구성된다
    
    * chore: (#17) Modal 컴포넌트 common 폴더로 이동
    
    * chore: (#17) Modal 컴포넌트 css property 정렬
    
    * design: (#17) Modal 컴포넌트 내 Body, Description css 수정
    
    * refactor: (#17) 파일 간 순환참조 코드 삭제
    
    * refactor: (#17) Modal 컴포넌트 onModalClose props에 setStateAction 대신 일반함수(closeModal) 전달하도록 수정
    
    * chore: (#17) 오타 수정
    
    * refactor: (#17) story에서만 사용하는 style들 stories.tsx 로 이동
    
    * refactor: (#17) size props의 타입 분리 대신 컴포넌트 파일 내에 명시
    
    * refactor: (#17) 모달 사이즈 상수화
    
    * chore: (#17) 불필요한 export 삭제

---
+++ frontend/src/components/common/Modal/style.ts
@@ -0,0 +1,45 @@
+import { styled } from 'styled-components';
+
+const modalSize: {
+  [key: string]: string;
+} = {
+  sm: '290px',
+  md: '590px',
+  lg: '700px',
+};
+
+export const All = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  background: rgba(0, 0, 0, 0.35);
+`;
+
+export const Container = styled.div<{ size: string }>`
+  display: grid;
+  grid-template-rows: 1fr 6fr;
+
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  width: ${props => modalSize[props.size]};
+  height: 290px;
+  border-radius: 12px;
+  border: 2px solid #f6f6f6;
+  padding: 5px;
+
+  background-color: white;
+
+  box-shadow: 0 0 6px 0 rgba(0, 0, 0, 0.5);
+`;

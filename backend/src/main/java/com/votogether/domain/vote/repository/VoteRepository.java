baade14a66eca3efe727706e0350607a819a3dd7
aiaiaiai1 <cvc100326@naver.com>
2023-07-13 16:31:06 +0900

    feat: (#36) Vote 리파지터리 추가

---
+++ backend/src/main/java/com/votogether/domain/vote/repository/VoteRepository.java
@@ -0,0 +1,7 @@
+package com.votogether.domain.vote.repository;
+
+import com.votogether.domain.vote.entity.Vote;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VoteRepository extends JpaRepository<Vote, Long> {
+}

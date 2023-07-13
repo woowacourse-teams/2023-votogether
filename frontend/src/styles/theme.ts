8dfb4b8b9d6c75cb9502ca4a56cb3eb8c5429154
inyeong-kang <tomatorkd@naver.com>
2023-07-13 17:13:59 +0900

    feat: (#38) theme 으로 공통 스타일링 관리

---
+++ frontend/src/styles/theme.ts
@@ -0,0 +1,20 @@
+import { DefaultTheme } from 'styled-components';
+
+const color = {
+  /** 
+  #333333
+  */
+  primary: '#FA7D7C',
+  /** 
+  #333333
+  */
+  white: '#FFFFFF',
+  /** 
+  #AAAAAA
+  */
+  secondary: '#AAAAAA',
+};
+
+export const theme: DefaultTheme = {
+  color,
+};

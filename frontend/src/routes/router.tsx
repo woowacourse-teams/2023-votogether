711bf88d29e9b41fa0b8e683d1613f891bc0178d
inyeong-kang <tomatorkd@naver.com>
2023-07-14 18:29:31 +0900

    refactor: (#38) 각 페이지에 대한 URL 명시

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,24 +1,23 @@
 import { createBrowserRouter } from 'react-router-dom';
 
-import { PATH } from '@constants/path';
-
 import PostDetail from '@pages/Post/PostDetail';
 import PostList from '@pages/Post/PostList';
 import WritePost from '@pages/Post/WritePost';
 
 const router = createBrowserRouter([
   {
-    path: PATH.HOME,
+    path: '/',
     element: <PostList />,
-  },
-  {
-    path: PATH.POST_WRITE,
-    element: <WritePost />,
-  },
-  {
-    path: PATH.POST_DETAIL,
-    element: <PostDetail />,
+    children: [
+      {
+        path: 'posts/write',
+        element: <WritePost />,
+      },
+      {
+        path: 'posts/:postId',
+        element: <PostDetail />,
+      },
+    ],
   },
 ]);
-
 export default router;

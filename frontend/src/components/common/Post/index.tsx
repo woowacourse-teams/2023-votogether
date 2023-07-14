2b6d322c2d13da0941274335d78b95d31c40b28b
chsua <csj1919@naver.com>
2023-07-14 23:38:35 +0900

    feat: (#24) 게시물 하나 컴포넌트 구현

---
+++ frontend/src/components/common/Post/index.tsx
@@ -0,0 +1,33 @@
+import { PostInfo } from '@type/post';
+
+import WrittenVoteOptionList from '@components/optionList/WrittenVoteOptionList';
+
+import * as S from './style';
+
+interface PostProps {
+  postInfo: PostInfo;
+  isPreview: boolean;
+}
+
+export default function Post({ postInfo, isPreview }: PostProps) {
+  return (
+    <S.Container>
+      <S.Category>{postInfo.category.map(category => category.name).join(' | ')}</S.Category>
+      <S.Title isPreview={isPreview}>{postInfo.title}</S.Title>
+      <S.Wrapper>
+        <S.Writer>{postInfo.writer.nick}</S.Writer>
+        <S.Wrapper>
+          <S.Time>{postInfo.startTime}</S.Time>
+          <S.Time>{postInfo.endTime}</S.Time>
+        </S.Wrapper>
+      </S.Wrapper>
+      <S.Content isPreview={isPreview}>{postInfo.content}</S.Content>
+      <WrittenVoteOptionList
+        selectedOptionId={postInfo.voteInfo.selectedOptionId}
+        handleVoteClick={() => {}}
+        isPreview={isPreview}
+        voteOptionList={postInfo.voteInfo.options}
+      />
+    </S.Container>
+  );
+}

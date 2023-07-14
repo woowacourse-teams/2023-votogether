1f36fed2c17fd496e5327945b093bb7d0109272c
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2023-07-14 18:10:23 +0900

    투표 작성 페이지의 투표 선택지 컴포넌트 UI 구현  (#40)
    * feat: (#20) 삭제, 파일 업로드 버튼 컴포넌트 UI 구현
    
    * feat: (#20) 이미지 업로드 버튼을 눌렀을 때 이미지 업로드 창이 나오도록 구현 및 파일명 변경
    
    * feat: (#20) 투표 선택지 아이템 컴포넌트 UI 구현
    
    * feat: (#20) 투표 선택지 작성 리스트 컴포넌트 UI 구현
    
    * feat: (#20) 훅 테스트 코드 작성 시작
    
    * test: (#20) 투표 선택지 작성에 사용하는 훅 테스트 작성
    
    * feat: (#20) 투표 선택지 작성 훅 구현
    
    * feat: (#20) 투표 선택지 작성 훅 적용 및 UI 구현
    
    * feat: (#20) 50자 이상 적었을 때 사용자에게 안내 기능 구현
    
    * feat: (#20) 사진의 이미지가 5MB가 넘어갈 경우 유저에게 안내하도록 구현
    
    * design: (#20): 삭제 버튼을 감싼 태그가 항상 왼쪽의 공간을 차지하도록 CSS 변경
    
    * refactor: (#20) svg 코드를 assets 폴더로 이동 후 import 하여 사용하도록 수정
    회색 버튼을 cssText로 관리하여 공통으로 관리하도록 수정
    
    * refactor: (#20) 코드 가독성을 위한 함수명, 변수명 수정
    
    * design: (#20) 화면 크기에 따라 폰트, 버튼 사이즈 변경되도록 구현
    
    * style: (#20) CSS 속성 순서 변경 및 불필요한 타입 선언 제거
    
    * chore: (#20) 함수 동작 과정에 대한 설명 주석 추가
    
    * chore: (#20) 테스트 문구 변경

---
+++ frontend/src/components/optionList/WritingVoteOptionList/WritingVoteOption/OptionUploadImageButton/OptionUploadImageButton.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import OptionUploadImageButton from '.';
+
+const meta: Meta<typeof OptionUploadImageButton> = {
+  component: OptionUploadImageButton,
+};
+
+export default meta;
+type Story = StoryObj<typeof OptionUploadImageButton>;
+
+export const Default: Story = {
+  render: () => <OptionUploadImageButton labelId={123} />,
+};
---
+++ frontend/src/hooks/useWritingOption.tsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+
+export interface WritingVoteOptionType {
+  id: number;
+  text: string;
+  imageUrl?: string;
+}
+
+const MIN_COUNT = 2;
+const MAX_COUNT = 5;
+
+const MAX_FILE_SIZE = 5000000;
+
+const INIT_OPTION_LIST = [
+  { id: Math.floor(Math.random() * 100000), text: '', imageUrl: '' },
+  { id: Math.floor(Math.random() * 100000), text: '', imageUrl: '' },
+];
+
+export const useWritingOption = (initialOptionList: WritingVoteOptionType[] = INIT_OPTION_LIST) => {
+  const [optionList, setOptionList] = useState(initialOptionList);
+
+  const addOption = () => {
+    if (optionList.length >= MAX_COUNT) return;
+
+    const updatedOptionList = [
+      ...optionList,
+      { id: Math.floor(Math.random() * 100000), text: '', imageUrl: '' },
+    ];
+
+    setOptionList(updatedOptionList);
+  };
+
+  const deleteOption = (optionId: number) => {
+    if (optionList.length <= MIN_COUNT) return;
+
+    const removedOptionList = optionList.filter(optionItem => optionItem.id !== optionId);
+
+    setOptionList(removedOptionList);
+  };
+
+  const removeImage = (optionId: number) => {
+    const updatedOptionList = optionList.map(optionItem => {
+      if (optionItem.id === optionId) {
+        return { ...optionItem, imageUrl: '' };
+      }
+
+      return optionItem;
+    });
+
+    setOptionList(updatedOptionList);
+  };
+
+  const handleUploadImage = (event: React.ChangeEvent<HTMLInputElement>, optionId: number) => {
+    const { files } = event.target;
+
+    if (!files) return;
+
+    const file = files[0];
+
+    event.target.setCustomValidity('');
+
+    if (file.size > MAX_FILE_SIZE) {
+      event.target.setCustomValidity('사진의 용량은 5MB 이하만 가능합니다.');
+      event.target.reportValidity();
+
+      return;
+    }
+
+    const reader = new FileReader();
+
+    // readAsDataURL 메서드를 통해 파일을 모두 읽고 나면 reader의 loadend 이벤트에서 이미지 미리보기 결과를 확인할 수 있습니다.
+    reader.readAsDataURL(file);
+
+    reader.onloadend = () => {
+      const updatedOptionList = optionList.map(optionItem => {
+        if (optionItem.id === optionId) {
+          return { ...optionItem, imageUrl: reader.result?.toString() };
+        }
+
+        return optionItem;
+      });
+
+      setOptionList(updatedOptionList);
+    };
+  };
+
+  return { optionList, addOption, deleteOption, removeImage, handleUploadImage };
+};

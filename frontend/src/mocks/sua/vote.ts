cb1a1ba0305b3be4b383ef8747b34007693612dc
chsua <csj1919@naver.com>
2023-07-15 16:16:15 +0900

    fix: 오타로 인한 patch mock이 안되는 오류 수정
    - 원인: url 오타

--- frontend/src/mocks/sua/vote.ts
+++ frontend/src/mocks/sua/vote.ts
@@ -23,19 +23,19 @@ export const votePostTest = [
   }),
 
   //선택지 수정
-  rest.patch(`/posts/1111112//options?source=12312&target=12314`, (req, res, ctx) => {
+  rest.patch(`/posts/1111112/options?source=12312&target=12314`, (req, res, ctx) => {
     console.log('투표 변경ㅇㅋ');
 
     return res(ctx.status(200));
   }),
 
-  rest.patch(`/posts/1111112//options?source=12312&target=123152`, (req, res, ctx) => {
+  rest.patch(`/posts/1111112/options?source=12312&target=123152`, (req, res, ctx) => {
     console.log('투표 변경ㅇㅋ');
 
     return res(ctx.status(200));
   }),
 
-  rest.patch(`/posts/1111112//options?source=12312&target=123122`, (req, res, ctx) => {
+  rest.patch(`/posts/1111112/options?source=12312&target=123122`, (req, res, ctx) => {
     console.log('투표 변경ㅇㅋ');
 
     return res(ctx.status(200));

4c683af1d681835be061461d36bf29dea4e8a3f0
JunH <tjdtls690@gmail.com>
2023-07-15 20:02:45 +0900

    feat: (#32) swagger 이미지 파일 테스트가 가능하도록 환경 설정

---
+++ backend/src/main/java/com/votogether/config/SwaggerBeanConfig.java
@@ -0,0 +1,17 @@
+package com.votogether.config;
+
+import java.util.ArrayList;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+@Configuration
+public class SwaggerBeanConfig {
+    
+    public SwaggerBeanConfig(MappingJackson2HttpMessageConverter converter) {
+        var supportedMediaTypes = new ArrayList<>(converter.getSupportedMediaTypes());
+        supportedMediaTypes.add(new MediaType("application", "octet-stream"));
+        converter.setSupportedMediaTypes(supportedMediaTypes);
+    }
+
+}

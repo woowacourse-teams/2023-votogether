ef1a7f833bac4c1a6bce966c973c09d3cc8cfa71
chsua <csj1919@naver.com>
2023-07-15 16:39:42 +0900

    style: (#24) 불필요한 코드 삭제

--- frontend/src/api/sua/post.ts
+++ frontend/src/api/sua/post.ts
@@ -1,7 +1,7 @@
 import { patchFetch, postFetch } from '@utils/fetch';
 
 export const votePost = async (postId: number, optionId: number) => {
-  return await postFetch<'', ''>(`/posts/${postId}/options/${optionId}`, '');
+  return await postFetch(`/posts/${postId}/options/${optionId}`, '');
 };
 
 export const changeVotedOption = async (

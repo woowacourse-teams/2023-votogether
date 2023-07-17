5a809d1c6e34a43766f93e5b3442d9cc5d221309
aiaiaiai1 <cvc100326@naver.com>
2023-07-18 01:34:21 +0900

    feat: (#50) Swagger 어노테이션 추가

--- backend/src/main/java/com/votogether/domain/category/contorller/CategoryController.java
+++ backend/src/main/java/com/votogether/domain/category/contorller/CategoryController.java
@@ -2,6 +2,9 @@ package com.votogether.domain.category.contorller;
 
 import com.votogether.domain.category.dto.response.CategoryResponse;
 import com.votogether.domain.category.service.CategoryService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,12 +14,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "카테고리", description = "카테고리 API")
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoryController {
 
     private final CategoryService categoryService;
 
+    @Operation(summary = "카테고리 조회하기", description = "전체 카테고리 목록을 조회한다.")
+    @ApiResponse(responseCode = "200", description = "조회 성공")
     @GetMapping("/guest")
     public ResponseEntity<List<CategoryResponse>> getAllCategories() {
         List<CategoryResponse> categories = categoryService.getAllCategories();

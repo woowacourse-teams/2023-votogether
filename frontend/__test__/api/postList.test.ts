b0352c1760458a682f924df18896f2c836e35c0d
gilpop8663 <wolfye@naver.com>
2023-07-19 02:27:51 +0900

    feat: (#61) 게시글 목록을 게시글 진행 상태, 정렬 옵션에 따라 요청을 다르게 보내도록 구현

--- frontend/__test__/api/postList.test.ts
+++ frontend/__test__/api/postList.test.ts
@@ -3,8 +3,38 @@ import { getPostList } from '@api/wus/postList';
 import { MOCK_POST_LIST } from '@mocks/mockData/postList';
 
 describe('전체 게시글 목록을 패치하는 로직이 의도한대로 작동하는 지 확인한다.', () => {
-  test('게시글 목록을 불러온다.', async () => {
-    const data = await getPostList();
+  test('게시글 목록을 모든 게시글을 인기순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'all', postSorting: 'popular' });
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+
+  test('게시글 목록을 모든 게시글, 최신순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'all', postSorting: 'latest' });
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+
+  test('게시글 목록을 투표 진행 중인 게시글, 인기순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'progress', postSorting: 'popular' });
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+
+  test('게시글 목록을 투표 진행 중인 게시글, 최신순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'progress', postSorting: 'latest' });
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+
+  test('게시글 목록을 투표 마감한 게시글, 인기순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'closed', postSorting: 'popular' });
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+
+  test('게시글 목록을 투표 마감한 게시글, 최신순으로 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'closed', postSorting: 'latest' });
 
     expect(data).toEqual(MOCK_POST_LIST);
   });
--- frontend/src/api/wus/postList.ts
+++ frontend/src/api/wus/postList.ts
@@ -1,7 +1,17 @@
 import { PostInfo } from '@type/post';
 
+import { PostStatusType, SortingOptionType } from '@components/post/PostListPage/constants/option';
+
 import { getFetch } from '@utils/fetch';
 
-export const getPostList = async () => {
-  return await getFetch<PostInfo[]>('/posts');
+interface GetPostListParams {
+  postStatus: PostStatusType;
+  postSorting: SortingOptionType;
+}
+
+export const getPostList = async ({ postStatus, postSorting }: GetPostListParams) => {
+  const status = postStatus === 'progress' ? 'true' : 'false';
+  const sorting = postSorting === 'popular' ? 'hot' : 'latest';
+
+  return await getFetch<PostInfo[]>(`/posts?status=${status}&sorting=${sorting}`);
 };

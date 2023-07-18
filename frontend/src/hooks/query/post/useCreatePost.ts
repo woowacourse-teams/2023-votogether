9983e913810d6179a0d55f4f81c0af174b584bca
inyeong-kang <tomatorkd@naver.com>
2023-07-18 22:46:31 +0900

    refactor: (#55) request로 보낼 데이터의 타입을 FormData로 변경

--- frontend/src/hooks/query/post/useCreatePost.ts
+++ frontend/src/hooks/query/post/useCreatePost.ts
@@ -1,23 +1,18 @@
 import { QUERY_KEY } from '@constants/queryKey';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { PostRequest } from '@type/post';
-
 import { createPost } from '@api/jero/post';
 
 export const useCreatePost = () => {
   const queryClient = useQueryClient();
-  const { mutate, isLoading, isError, error } = useMutation(
-    (post: PostRequest) => createPost(post),
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries([QUERY_KEY.posts]);
-      },
-      onError: () => {
-        window.console.log('createPost error');
-      },
-    }
-  );
+  const { mutate, isLoading, isError, error } = useMutation((post: FormData) => createPost(post), {
+    onSuccess: () => {
+      queryClient.invalidateQueries([QUERY_KEY.posts]);
+    },
+    onError: () => {
+      window.console.log('createPost error');
+    },
+  });
 
   return { mutate, isLoading, isError, error };
 };
--- frontend/src/hooks/query/post/useEditPost.ts
+++ frontend/src/hooks/query/post/useEditPost.ts
@@ -1,14 +1,12 @@
 import { QUERY_KEY } from '@constants/queryKey';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
-import { PostRequest } from '@type/post';
-
 import { editPost } from '@api/jero/post';
 
 export const useEditPost = (postId: number) => {
   const queryClient = useQueryClient();
   const { mutate, isLoading, isError, error } = useMutation(
-    (updatedPost: PostRequest) => editPost(postId, updatedPost),
+    (updatedPost: FormData) => editPost(postId, updatedPost),
     {
       onSuccess: () => {
         queryClient.invalidateQueries([QUERY_KEY.posts, postId]);

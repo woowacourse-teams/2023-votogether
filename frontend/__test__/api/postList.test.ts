6123af042f3e6888a5969675c3abdd85688085e0
Gilpop8663 <wolfye@naver.com>
2023-07-18 14:18:02 +0900

    test: 전체 게시글 목록 패치하는 훅과 함수 테스트 작성

---
+++ frontend/__test__/api/postList.test.ts
@@ -0,0 +1,11 @@
+import { getPostList } from '@api/wus/postList';
+
+import { MOCK_POST_LIST } from '../../src/mocks/wus/post';
+
+describe('전체 게시글 목록을 패치하는 로직이 의도한대로 작동하는 지 확인한다.', () => {
+  test('게시글 목록을 불러온다.', async () => {
+    const data = await getPostList();
+
+    expect(data).toEqual(MOCK_POST_LIST);
+  });
+});
--- frontend/__test__/hooks/usePostList.test.tsx
+++ frontend/__test__/hooks/usePostList.test.tsx
@@ -1,14 +1,24 @@
-import { renderHook } from '@testing-library/react';
+import React, { ReactNode } from 'react';
 
-import { usePostList } from '../../src/hooks/query/usePostList';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { renderHook, waitFor } from '@testing-library/react';
+
+import { usePostList } from '@hooks/query/usePostList';
+
 import { MOCK_POST_LIST } from '../../src/mocks/wus/post';
 
+const queryClient = new QueryClient();
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
 describe('usePostList 훅이 의도한대로 작동하는 지 확인한다.', () => {
-  test('usePostList에서 패칭된 값을 확인한다.', () => {
-    const { result } = renderHook(() => usePostList());
+  test('usePostList에서 패칭된 값을 확인한다.', async () => {
+    const { result } = renderHook(() => usePostList(), {
+      wrapper,
+    });
 
-    const { data } = result.current;
-
-    expect(data).toEqual(MOCK_POST_LIST);
+    await waitFor(() => expect(result.current.data).toEqual(MOCK_POST_LIST));
   });
 });

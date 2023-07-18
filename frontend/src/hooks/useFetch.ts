7570726162f47969e9fbc932e894902a065f956b
chsua <csj1919@naver.com>
2023-07-18 15:43:41 +0900

    refactor: (#65) useFetch 내 데이터이름 범용성 향성을 위해 수정

--- frontend/src/hooks/useFetch.ts
+++ frontend/src/hooks/useFetch.ts
@@ -1,14 +1,14 @@
 import { useEffect, useState } from 'react';
 
 export const useFetch = <T>(fetchFn: (parameter: any) => Promise<T>, parameter: any) => {
-  const [postDetail, setPostDetail] = useState<T>();
+  const [data, setData] = useState<T>();
   const [errorMessage, setErrorMessage] = useState<string>();
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchFn(parameter)
       .then(res => {
-        setPostDetail(res);
+        setData(res);
       })
       .catch(rej => {
         setErrorMessage(rej.message);
@@ -18,11 +18,11 @@ export const useFetch = <T>(fetchFn: (parameter: any) => Promise<T>, parameter: 
       });
 
     return (() => {
-      setPostDetail(undefined);
+      setData(undefined);
       setIsLoading(true);
       setErrorMessage(undefined);
     })();
   }, []);
 
-  return { postDetail, errorMessage, isLoading };
+  return { data, errorMessage, isLoading };
 };

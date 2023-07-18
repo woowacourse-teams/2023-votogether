8c2cd7b68f324e18114aa2de62a6b7cf76ac4a3c
aiaiaiai1 <cvc100326@naver.com>
2023-07-18 13:38:18 +0900

    test: (#66) Repository 테스트 추가
    - 아이디를 통해 카테고리 조회 테스트
    - 멤버카테고리 저장 테스트

---
+++ backend/src/test/java/com/votogether/domain/member/repository/MemberCategoryRepositoryTest.java
@@ -0,0 +1,62 @@
+package com.votogether.domain.member.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.votogether.domain.RepositoryTest;
+import com.votogether.domain.category.entity.Category;
+import com.votogether.domain.category.repository.CategoryRepository;
+import com.votogether.domain.member.entity.Gender;
+import com.votogether.domain.member.entity.Member;
+import com.votogether.domain.member.entity.MemberCategory;
+import com.votogether.domain.member.entity.SocialType;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@RepositoryTest
+class MemberCategoryRepositoryTest {
+
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    MemberCategoryRepository memberCategoryRepository;
+
+    @Test
+    @DisplayName("멤버가 선호하는 카테고리를 저장한다.")
+    void save() {
+        // given
+        Category category = Category.builder()
+                .name("개발")
+                .build();
+
+        Member member = Member.builder()
+                .gender(Gender.MALE)
+                .point(0)
+                .socialType(SocialType.GOOGLE)
+                .nickname("user1")
+                .socialId("kakao@gmail.com")
+                .birthDate(
+                        LocalDateTime.of(1995, 07, 12, 00, 00))
+                .build();
+
+        categoryRepository.save(category);
+        memberRepository.save(member);
+
+        MemberCategory memberCategory = MemberCategory.builder()
+                .member(member)
+                .category(category)
+                .build();
+
+        // when
+        memberCategoryRepository.save(memberCategory);
+
+        // then
+        assertThat(memberCategory.getId()).isNotNull();
+    }
+
+}

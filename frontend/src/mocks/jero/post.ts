5a729f312d0717b80c14eacd2de7e802058e2660
inyeong-kang <tomatorkd@naver.com>
2023-07-18 22:47:39 +0900

    refactor: (#55) mocking 함수 url, 상태 코드 수정

--- frontend/src/mocks/jero/post.ts
+++ frontend/src/mocks/jero/post.ts
@@ -13,12 +13,12 @@ export const createEditPostTest = [
   }),
 
   //게시글 수정
-  rest.put('/posts/1', (req, res, ctx) => {
+  rest.put('/posts/:postId', (req, res, ctx) => {
     window.console.log('게시글 수정 완료', req.body);
 
     return res(
       ctx.delay(1000),
-      ctx.status(201),
+      ctx.status(200),
       ctx.json({ message: '게시글이 성공적으로 수정되었습니다!!' })
     );
   }),

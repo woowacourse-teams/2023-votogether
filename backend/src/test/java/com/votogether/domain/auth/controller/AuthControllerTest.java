b9f6d3eb51be01d96e85aaaa6e0bb015c9d74a85
jeomxon <jeomxon@gmail.com>
2023-07-18 22:43:39 +0900

    test: (#29) loginWithKakao메서드에 대한 컨트롤러 단위 테스트 작성

---
+++ backend/src/test/java/com/votogether/domain/auth/controller/AuthControllerTest.java
@@ -0,0 +1,66 @@
+package com.votogether.domain.auth.controller;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+import com.votogether.domain.auth.service.AuthService;
+import com.votogether.domain.member.service.MemberService;
+import com.votogether.global.jwt.TokenProcessor;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = AuthController.class)
+class AuthControllerTest {
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @MockBean
+    AuthService authService;
+
+    @MockBean
+    TokenProcessor tokenProcessor;
+
+    @MockBean
+    MemberService memberService;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.standaloneSetup(new AuthController(authService));
+    }
+
+    @Test
+    @DisplayName("카카오 로그인을 한다.")
+    void loginWithKakao() {
+        // given
+        String accessToken = "abcdefg";
+
+        given(authService.register(any())).willReturn(accessToken);
+
+        // when
+        String responseBody = RestAssuredMockMvc
+                .given().log().all()
+                .queryParam("code", "abc1234")
+                .when().get("/auth/kakao/callback")
+                .then().log().all()
+                .status(HttpStatus.OK)
+                .extract()
+                .asString();
+
+        // then
+        assertAll(
+                () -> assertThat(responseBody).contains("accessToken"),
+                () -> assertThat(responseBody).contains(accessToken)
+        );
+    }
+
+}

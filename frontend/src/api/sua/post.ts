cd6d10b99320c1e6bcf89eee8a3a79e50eed0331
chsua <csj1919@naver.com>
2023-07-18 13:21:37 +0900

    feat: (#65) 게시물 하나 상세정보 fetch 함수 구현

--- frontend/src/api/sua/post.ts
+++ frontend/src/api/sua/post.ts
@@ -1,14 +1,22 @@
-import { patchFetch, postFetch } from '@utils/fetch';
+import { PostInfo } from '@type/post';
+
+import { getFetch, patchFetch, postFetch } from '@utils/fetch';
 
 export const votePost = async (postId: number, optionId: number) => {
   return await postFetch(`/posts/${postId}/options/${optionId}`, '');
 };
 
-export const changeVotedOption = async (
-  postId: number,
-  { originOptionId, newOptionId }: { originOptionId: number; newOptionId: number }
-) => {
+interface ChangeVotedOptionProps {
+  postId: number;
+  optionData: { originOptionId: number; newOptionId: number };
+}
+
+export const changeVotedOption = async ({ postId, optionData }: ChangeVotedOptionProps) => {
   return await patchFetch(
-    `/posts/${postId}/options?source=${originOptionId}&target=${newOptionId}`
+    `/posts/${postId}/options?source=${optionData.originOptionId}&target=${optionData.newOptionId}`
   );
+};
+
+export const getVoteDetail = async (postId: number): Promise<PostInfo> => {
+  return await getFetch<PostInfo>(`/posts/${postId}`);
 };

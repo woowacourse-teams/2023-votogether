34d94bf1b3dafb4d54a84a43c3d6042a55ffab2f
inyeong-kang <tomatorkd@naver.com>
2023-07-19 13:01:49 +0900

    chore: (#55) 불필요한 코드 삭제

--- frontend/src/pages/post/PostList.tsx
+++ frontend/src/pages/post/PostList.tsx
@@ -1,29 +1,12 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
 
-import AddButton from '@components/common/AddButton';
 import Example from '@components/Example';
 
 export default function PostList() {
-  const navigate = useNavigate();
-  const postId = 1;
-
   return (
     <div>
       PostList 글 목록 페이지
       <Example />
-      <AddButton
-        size="sm"
-        onClick={() => {
-          navigate('/posts/write');
-        }}
-      />
-      <AddButton
-        size="lg"
-        onClick={() => {
-          navigate(`/posts/write/${postId}`);
-        }}
-      />
     </div>
   );
 }

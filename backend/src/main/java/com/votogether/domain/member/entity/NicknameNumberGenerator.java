3f4a538fdedf8150ce0e14eb8424d8e78e6ebe1b
jeomxon <jeomxon@gmail.com>
2023-07-19 17:23:54 +0900

    fix: (#29) 멤버의 이름에 포함되는 숫자가 고정되는 문제 해결

--- backend/src/main/java/com/votogether/domain/member/entity/NicknameNumberGenerator.java
+++ backend/src/main/java/com/votogether/domain/member/entity/NicknameNumberGenerator.java
@@ -2,11 +2,11 @@ package com.votogether.domain.member.entity;
 
 public class NicknameNumberGenerator implements NumberGenerator {
 
-    private int number = 0;
+    private static int number = 0;
 
     @Override
     public int generate() {
         return ++number;
     }
-    
+
 }

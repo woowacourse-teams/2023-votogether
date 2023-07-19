9e7ab3a51a0749bb97b39d0b45918abc0a22acac
woo-chang <clllickme@naver.com>
2023-07-19 13:43:00 +0900

    feat: (#77) 게시글 투표 옵션에 대한 투표 통계 조회 API 구현

--- backend/src/test/java/com/votogether/domain/post/controller/PostControllerTest.java
+++ backend/src/test/java/com/votogether/domain/post/controller/PostControllerTest.java
@@ -4,10 +4,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.startsWith;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.votogether.domain.post.dto.request.PostCreateRequest;
+import com.votogether.domain.post.dto.response.VoteCountForAgeGroupResponse;
+import com.votogether.domain.post.dto.response.VoteOptionStatisticsResponse;
 import com.votogether.domain.post.service.PostService;
 import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -16,6 +19,7 @@ import io.restassured.response.ExtractableResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -38,27 +42,27 @@ class PostControllerTest {
     @DisplayName("게시글을 등록한다")
     void save() throws IOException {
         // given
-        final PostCreateRequest postCreateRequest = PostCreateRequest.builder().build();
+        PostCreateRequest postCreateRequest = PostCreateRequest.builder().build();
 
-        final String fileName1 = "testImage1.PNG";
-        final String resultFileName1 = "testResultImage1.PNG";
-        final String filePath1 = "src/test/resources/images/" + fileName1;
+        String fileName1 = "testImage1.PNG";
+        String resultFileName1 = "testResultImage1.PNG";
+        String filePath1 = "src/test/resources/images/" + fileName1;
         File file1 = new File(filePath1);
 
-        final String fileName2 = "testImage2.PNG";
-        final String resultFileName2 = "testResultImage2.PNG";
-        final String filePath2 = "src/test/resources/images/" + fileName2;
+        String fileName2 = "testImage2.PNG";
+        String resultFileName2 = "testResultImage2.PNG";
+        String filePath2 = "src/test/resources/images/" + fileName2;
         File file2 = new File(filePath2);
 
-        final ObjectMapper objectMapper = new ObjectMapper();
-        final String postRequestJson = objectMapper.writeValueAsString(postCreateRequest);
+        ObjectMapper objectMapper = new ObjectMapper();
+        String postRequestJson = objectMapper.writeValueAsString(postCreateRequest);
 
-        final long savedPostId = 1L;
+        long savedPostId = 1L;
         given(postService.save(any(), any(), anyList())).willReturn(savedPostId);
 
         // when, then
-        final String locationStartsWith = "/posts/";
-        final ExtractableResponse<MockMvcResponse> response = RestAssuredMockMvc.given().log().all()
+        String locationStartsWith = "/posts/";
+        ExtractableResponse<MockMvcResponse> response = RestAssuredMockMvc.given().log().all()
                 .contentType(ContentType.MULTIPART)
                 .multiPart("request", postRequestJson, "application/json")
                 .multiPart("images", resultFileName1, new FileInputStream(file1), "image/png")
@@ -69,8 +73,40 @@ class PostControllerTest {
                 .header("Location", startsWith(locationStartsWith))
                 .extract();
 
-        final String postId = response.header("Location").substring(locationStartsWith.length());
+        String postId = response.header("Location").substring(locationStartsWith.length());
         assertThat(Long.parseLong(postId)).isEqualTo(savedPostId);
+    }
+
+    @Test
+    @DisplayName("게시글 투표 옵션에 대한 투표 통계를 조회한다.")
+    void getVoteOptionStatistics() {
+        // given
+        VoteOptionStatisticsResponse response = new VoteOptionStatisticsResponse(
+                17,
+                10,
+                7,
+                List.of(
+                        new VoteCountForAgeGroupResponse("10대 미만", 2, 1, 1),
+                        new VoteCountForAgeGroupResponse("10대", 3, 1, 2),
+                        new VoteCountForAgeGroupResponse("20대", 2, 2, 0),
+                        new VoteCountForAgeGroupResponse("30대", 5, 3, 2),
+                        new VoteCountForAgeGroupResponse("40대", 1, 1, 0),
+                        new VoteCountForAgeGroupResponse("50대", 0, 0, 0),
+                        new VoteCountForAgeGroupResponse("60대 이상", 4, 2, 2)
+                )
+        );
+        given(postService.getVoteOptionStatistics(anyLong(), anyLong())).willReturn(response);
+
+        // when
+        VoteOptionStatisticsResponse result = RestAssuredMockMvc.given().log().all()
+                .when().get("/posts/{postId}/options/{optionId}", 1, 1)
+                .then().log().all()
+                .status(HttpStatus.OK)
+                .extract()
+                .as(VoteOptionStatisticsResponse.class);
+
+        // then
+        assertThat(result).usingRecursiveComparison().isEqualTo(response);
     }
 
 }

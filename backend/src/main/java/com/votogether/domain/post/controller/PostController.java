88db5b24dc026cfe19d785fa430ad312dd8be1e7
aiaiaiai1 <cvc100326@naver.com>
2023-07-19 11:43:21 +0900

    feat: (#80) Swagger 어노테이션 추가

--- backend/src/main/java/com/votogether/domain/post/controller/PostController.java
+++ backend/src/main/java/com/votogether/domain/post/controller/PostController.java
@@ -59,9 +59,11 @@ public class PostController {
 
 
     // TODO: 2023/07/19 응답값 DTO로 변환해주기
+    @Operation(summary = "투표한 게시글 조회", description = "회원이 투표한 게시글 목록을 최신순으로 조회한다.")
+    @ApiResponse(responseCode = "200", description = "조회 성공")
     @GetMapping("/votes/me")
     public ResponseEntity<List<Post>> getPostsVotedOn(final Member member) {
-        List<Post> postsVotedOn = postService.getPostsVotedOn(member);
+        final List<Post> postsVotedOn = postService.getPostsVotedOn(member);
         return ResponseEntity.status(HttpStatus.OK).body(postsVotedOn);
     }
 

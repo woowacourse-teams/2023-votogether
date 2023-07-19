262adbbacbaeec26c443badad8e4c35957b15010
chsua <csj1919@naver.com>
2023-07-19 13:03:03 +0900

    feat: (#78) 게시글 get, 게시글 삭제, 조기종료 mocking을 위해 설정

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,4 +1,5 @@
 import { example } from './example/get';
+import { getPostTest } from './sua/post';
 import { votePostTest } from './sua/vote';
 
-export const handlers = [...example, ...votePostTest];
+export const handlers = [...example, ...votePostTest, ...getPostTest];

3a07b70fff825005b4f6e29a77a242e71229c5a3
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2023-07-19 11:14:03 +0900

    공용 Select 컴포넌트 UI 구현 (#58)
    * feat: (#43) 셀렉트 컴포넌트 UI 구현
    스토리북 작성, 글로벌 CSS 설정, svg 파일 추가
    
    * feat: (#43) 셀렉트 컴포넌트 사용 예시 스토리북 작성
    셀렉트 부모에서 width 값을 지정해서 사용하도록 수정
    
    * refactor: (#43) 코드 가독성을 위한 타입, 변수명 수정
    
    * refactor: (#43) 셀렉트 컴포넌트에서 제네릭 타입을 받아서 사용하도록 수정
    타입스크립트의 제네릭을 통해 안정성을 더하였음
    
    ---------
    
    Co-authored-by: chsua <113416448+chsua@users.noreply.github.com>

---
+++ frontend/src/components/common/Select/index.tsx
@@ -0,0 +1,66 @@
+import React, { useState } from 'react';
+
+import chevronDown from '@assets/chevron-down.svg';
+import chevronUp from '@assets/chevron-up.svg';
+
+import { SELECT_DEFAULT, SELECT_DISABLED, SELECT_SELECTED } from './constants';
+import * as S from './style';
+
+export interface SelectProps<T extends string>
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  selectedOption: string;
+  optionList: Record<T, string>;
+  handleOptionChange: (option: T) => void;
+  isDisabled?: boolean;
+}
+
+export default function Select<T extends string>({
+  selectedOption,
+  optionList,
+  handleOptionChange,
+  isDisabled = false,
+  ...rest
+}: SelectProps<T>) {
+  const optionKeyList = Object.keys(optionList) as T[];
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleOpen = () => {
+    if (isDisabled) return;
+    setIsOpen(prev => !prev);
+  };
+
+  const handleSelectClick = (option: T) => {
+    handleOptionChange(option);
+    setIsOpen(false);
+  };
+
+  const getSelectStatus = () => {
+    if (isDisabled) {
+      return SELECT_DISABLED;
+    }
+
+    if (isOpen) {
+      return SELECT_SELECTED;
+    }
+
+    return SELECT_DEFAULT;
+  };
+
+  return (
+    <S.Container>
+      <S.SelectedContainer onClick={toggleOpen} $status={getSelectStatus()} {...rest}>
+        <span>{selectedOption}</span>
+        <S.Image src={isOpen ? chevronUp : chevronDown} alt="" $isSelected={isOpen} />
+      </S.SelectedContainer>
+      {isOpen && (
+        <S.OptionListContainer>
+          {optionKeyList.map(optionKey => (
+            <S.OptionContainer key={optionKey} onClick={() => handleSelectClick(optionKey)}>
+              {optionList[optionKey]}
+            </S.OptionContainer>
+          ))}
+        </S.OptionListContainer>
+      )}
+    </S.Container>
+  );
+}

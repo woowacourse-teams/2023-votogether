ba40654e9b2766e81e560b5a0026b9ddf8838229
Gilpop8663 <wolfye@naver.com>
2023-07-19 11:05:24 +0900

    feat: (#61) 전체 게시글 목록 패치할 때 페이지 번호를 받도록 수정

--- frontend/__test__/api/postList.test.ts
+++ frontend/__test__/api/postList.test.ts
@@ -4,38 +4,43 @@ import { MOCK_POST_LIST } from '@mocks/mockData/postList';
 
 describe('전체 게시글 목록을 패치하는 로직이 의도한대로 작동하는 지 확인한다.', () => {
   test('게시글 목록을 모든 게시글을 인기순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'all', postSorting: 'popular' });
+    const data = await getPostList({ postStatus: 'all', postSorting: 'popular', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 
   test('게시글 목록을 모든 게시글, 최신순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'all', postSorting: 'latest' });
+    const data = await getPostList({ postStatus: 'all', postSorting: 'latest', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 
   test('게시글 목록을 투표 진행 중인 게시글, 인기순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'progress', postSorting: 'popular' });
+    const data = await getPostList({ postStatus: 'progress', postSorting: 'popular', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 
   test('게시글 목록을 투표 진행 중인 게시글, 최신순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'progress', postSorting: 'latest' });
+    const data = await getPostList({ postStatus: 'progress', postSorting: 'latest', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 
   test('게시글 목록을 투표 마감한 게시글, 인기순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'closed', postSorting: 'popular' });
+    const data = await getPostList({ postStatus: 'closed', postSorting: 'popular', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 
   test('게시글 목록을 투표 마감한 게시글, 최신순으로 불러온다.', async () => {
-    const data = await getPostList({ postStatus: 'closed', postSorting: 'latest' });
+    const data = await getPostList({ postStatus: 'closed', postSorting: 'latest', pages: 0 });
 
-    expect(data).toEqual(MOCK_POST_LIST);
+    expect(data).toEqual(MOCK_POST_LIST[0]);
+  });
+  test('게시글 목록의 1페이지를 불러온다.', async () => {
+    const data = await getPostList({ postStatus: 'closed', postSorting: 'latest', pages: 1 });
+
+    expect(data).toEqual(MOCK_POST_LIST[0]);
   });
 });
--- frontend/src/api/wus/postList.ts
+++ frontend/src/api/wus/postList.ts
@@ -1,17 +1,34 @@
 import { PostInfo } from '@type/post';
 
-import { PostStatusType, PostSortingType } from '@components/post/PostListPage/constants/option';
+import {
+  PostStatusType,
+  PostSortingType,
+  STATUS_OPTION,
+  SORTING_OPTION,
+} from '@components/post/PostListPage/constants/option';
 
 import { getFetch } from '@utils/fetch';
 
 interface GetPostListParams {
   postStatus: PostStatusType;
   postSorting: PostSortingType;
+  pages: number;
 }
 
-export const getPostList = async ({ postStatus, postSorting }: GetPostListParams) => {
-  const status = postStatus === 'progress' ? 'true' : 'false';
-  const sorting = postSorting === 'popular' ? 'hot' : 'latest';
+const REQUEST_STATUS_OPTION = {
+  [STATUS_OPTION.all]: 'all',
+  [STATUS_OPTION.progress]: 'progress',
+  [STATUS_OPTION.closed]: 'closed',
+};
 
-  return await getFetch<PostInfo[]>(`/posts?status=${status}&sorting=${sorting}`);
+const REQUEST_SORTING_OPTION = {
+  [SORTING_OPTION.latest]: 'latest',
+  [SORTING_OPTION.popular]: 'hot',
+};
+
+export const getPostList = async ({ postStatus, postSorting, pages }: GetPostListParams) => {
+  const status = REQUEST_STATUS_OPTION[postStatus];
+  const sorting = REQUEST_SORTING_OPTION[postSorting];
+
+  return await getFetch<PostInfo[]>(`/posts?status=${status}&sorting=${sorting}&pages=${pages}`);
 };

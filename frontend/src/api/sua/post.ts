1912a58522e41653248ea1a28242a9118399858c
chsua <csj1919@naver.com>
2023-07-19 13:03:36 +0900

    feat: (#78) 게시글 get, 게시글 삭제, 조기종료 관련 api 작성

--- frontend/src/api/sua/post.ts
+++ frontend/src/api/sua/post.ts
@@ -1,4 +1,6 @@
-import { patchFetch, postFetch } from '@utils/fetch';
+import { PostInfo } from '@type/post';
+
+import { deleteFetch, getFetch, patchFetch, postFetch } from '@utils/fetch';
 
 export const votePost = async (postId: number, optionId: number) => {
   return await postFetch(`/posts/${postId}/options/${optionId}`, '');
@@ -11,4 +13,16 @@ export const changeVotedOption = async (
   return await patchFetch(
     `/posts/${postId}/options?source=${originOptionId}&target=${newOptionId}`
   );
+};
+
+export const getPost = async (postId: number): Promise<PostInfo> => {
+  return await getFetch<PostInfo>(`/posts/${postId}`);
+};
+
+export const removePost = async (postId: number) => {
+  return await deleteFetch(`/posts/${postId}`);
+};
+
+export const setEarlyClosePost = async (postId: number) => {
+  return await patchFetch(`/posts/${postId}/close`);
 };

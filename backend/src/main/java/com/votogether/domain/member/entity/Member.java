214d583d5d0ed3a31364f39b63bf14fb4c052f79
JunH <tjdtls690@gmail.com>
2023-07-20 12:06:28 +0900

    refactor: (#88) 수월한 디버깅을 위해 member toString 추가

--- backend/src/main/java/com/votogether/domain/member/entity/Member.java
+++ backend/src/main/java/com/votogether/domain/member/entity/Member.java
@@ -13,8 +13,10 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@ToString
 @Getter
 @Entity
 public class Member extends BaseEntity {

8b6613411ba21ad59410bad1c070860c07594fe8
woo-chang <clllickme@naver.com>
2023-07-20 11:15:17 +0900

    refactor: (#77) 변수 사용 시 변수명과 동일할 때 불필요한 어노테이션 제거

--- backend/src/main/java/com/votogether/domain/vote/repository/VoteRepository.java
+++ backend/src/main/java/com/votogether/domain/vote/repository/VoteRepository.java
@@ -8,16 +8,16 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface VoteRepository extends JpaRepository<Vote, Long> {
 
-    @Query("SELECT new com.votogether.domain.vote.dto.VoteStatus(m.ageRange, m.gender, COUNT(v)) " +
-            "FROM Vote v " +
-            "JOIN v.member m " +
-            "WHERE v.postOption.id = :postOptionId " +
-            "GROUP BY m.ageRange, m.gender")
-    List<VoteStatus> findVoteCountByPostOptionIdGroupByAgeRangeAndGender(@Param("postOptionId") Long postOptionId);
+    @Query("SELECT new com.votogether.domain.vote.dto.VoteStatus(m.ageRange, m.gender, COUNT(v))" +
+            " FROM Vote v" +
+            " JOIN v.member m" +
+            " WHERE v.postOption.id = :postOptionId" +
+            " GROUP BY m.ageRange, m.gender"
+    )
+    List<VoteStatus> findVoteCountByPostOptionIdGroupByAgeRangeAndGender(Long postOptionId);
 
     Optional<Vote> findByMemberAndPostOption(final Member member, final PostOption postOption);
 

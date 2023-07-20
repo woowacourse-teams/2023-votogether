411d0a001188bbefddef55422e549587b2fbae00
lookh <103165859+aiaiaiai1@users.noreply.github.com>
2023-07-20 10:54:34 +0900

    (회원) 선호 카테고리 삭제 기능 구현 (#79)
    * feat: (#67) (회원) 선호 카테고리 삭제 API 기능 구현
    
    * feat: (#67) Swagger 어노테이션 추가
    
    * test: (#67) Controller 단위테스트, Service 통합테스트 추가
    
    - 이전에 누락된 테스트 코드까지 추가함
    
    * style: (#67) final 키워드 추가
    
    * refactor: (#68) CategoryResponse 파라미터 값 수정
    
    * feat: (#68) (회원) 카테고리 목록 전체 조회 API 추가
    
    * teat: (#68) (회원) 레파지토리 테스트 추가
    
    * teat: (#67) 선호하는 카테고리에 없는 카테고리를 삭제하는 경우 예외 테스트 추가
    
    * refactor: (#67) 개행 및 스태틱 임포트 리펙터링
    
    * feat: (#67) Swagger 어노테이션 에러 응답 설명 추가
    
    * refactor: (#67) url 오타 수정
    
    * refactor: (#67) 개행 및 컨벤션 수정

--- backend/src/main/java/com/votogether/domain/category/contorller/CategoryController.java
+++ backend/src/main/java/com/votogether/domain/category/contorller/CategoryController.java
@@ -5,11 +5,13 @@ import com.votogether.domain.category.service.CategoryService;
 import com.votogether.domain.member.entity.Member;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,16 +30,38 @@ public class CategoryController {
     @ApiResponse(responseCode = "200", description = "조회 성공")
     @GetMapping("/guest")
     public ResponseEntity<List<CategoryResponse>> getAllCategories() {
-        List<CategoryResponse> categories = categoryService.getAllCategories();
+        final List<CategoryResponse> categories = categoryService.getAllCategories();
         return ResponseEntity.status(HttpStatus.OK).body(categories);
     }
 
     @Operation(summary = "선호 카테고리 추가하기", description = "선호하는 카테고리를 선호 카테고리 목록에 추가한다.")
-    @ApiResponse(responseCode = "201", description = "추가 성공")
+    @ApiResponses({
+            @ApiResponse(responseCode = "201", description = "추가 성공"),
+            @ApiResponse(responseCode = "400", description = "해당 카테고리가 추가가 되어 있어 중복 추가 실패"),
+            @ApiResponse(responseCode = "404", description = "해당 카테고리가 존재하지 않아 추가 실패"),
+    })
     @PostMapping("/{categoryId}/like")
     public ResponseEntity<Void> addFavoriteCategory(final Member member, @PathVariable final Long categoryId) {
         categoryService.addFavoriteCategory(member, categoryId);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @Operation(summary = "선호 카테고리 삭제하기", description = "선호하는 카테고리를 선호 카테고리 목록에서 삭제한다.")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "삭제 성공"),
+            @ApiResponse(responseCode = "400", description = "선호하는 카테고리가 아니여서 삭제 실패"),
+            @ApiResponse(responseCode = "404", description = "해당 카테고리가 존재하지 않아 삭제 실패")
+    })
+    @DeleteMapping("/{categoryId}/like")
+    public ResponseEntity<Void> removeFavoriteCategory(final Member member, @PathVariable final Long categoryId) {
+        categoryService.removeFavoriteCategory(member, categoryId);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Void> getAllCategories(final Member member) {
+        categoryService.getAllCategories(member);
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }

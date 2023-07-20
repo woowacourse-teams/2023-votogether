a9dd495db6a538f47d2709bf5370e435b941bbfe
Gilpop8663 <wolfye@naver.com>
2023-07-20 12:02:57 +0900

    feat: (#64) 카테고리 리스트 mock 데이터 선언

---
+++ frontend/src/mocks/mockData/categoryList.ts
@@ -0,0 +1,27 @@
+import { Category } from '@type/category';
+
+export const MOCK_CATEGORY_LIST: Category[] = [
+  { id: 1, name: '음식', isFavorite: false },
+  { id: 2, name: '연애', isFavorite: true },
+  { id: 3, name: '패션', isFavorite: false },
+  { id: 4, name: '금융', isFavorite: false },
+  { id: 5, name: '여행', isFavorite: false },
+  { id: 6, name: '게임', isFavorite: false },
+  { id: 7, name: '재테크', isFavorite: false },
+  { id: 8, name: '요리', isFavorite: true },
+  { id: 9, name: '개발', isFavorite: true },
+  { id: 10, name: '전자기기', isFavorite: true },
+];
+
+export const MOCK_GUEST_CATEGORY_LIST: Category[] = [
+  { id: 1, name: '음식', isFavorite: false },
+  { id: 2, name: '연애', isFavorite: false },
+  { id: 3, name: '패션', isFavorite: false },
+  { id: 4, name: '금융', isFavorite: false },
+  { id: 5, name: '여행', isFavorite: false },
+  { id: 6, name: '게임', isFavorite: false },
+  { id: 7, name: '재테크', isFavorite: false },
+  { id: 8, name: '요리', isFavorite: false },
+  { id: 9, name: '개발', isFavorite: false },
+  { id: 10, name: '전자기기', isFavorite: false },
+];

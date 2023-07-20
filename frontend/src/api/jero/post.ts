7765be523ee3529863285cb964b432d71a31daa9
Gilpop8663 <wolfye@naver.com>
2023-07-20 16:10:33 +0900

    refactor: (#94) api 폴더 통합

--- frontend/src/api/jero/post.ts
+++
@@ -1,9 +0,0 @@
-import { multiPutFetch, multiPostFetch } from '@utils/fetch';
-
-export const createPost = async (newPost: FormData) => {
-  return await multiPostFetch('/posts', newPost);
-};
-
-export const editPost = async (postId: number, updatedPost: FormData) => {
-  return await multiPutFetch(`/posts/${postId}`, updatedPost);
-};

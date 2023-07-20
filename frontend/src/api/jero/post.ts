d407a8370ad3c777bb1dd9904e23614c32def949
inyeong-kang <tomatorkd@naver.com>
2023-07-21 04:19:24 +0900

    chore: husky test - commit msg convention 되는지 체크하는 테스트1

--- frontend/src/api/jero/post.ts
+++ frontend/src/api/jero/post.ts
@@ -7,3 +7,5 @@ export const createPost = async (newPost: FormData) => {
 export const editPost = async (postId: number, updatedPost: FormData) => {
   return await multiPutFetch(`/posts/${postId}`, updatedPost);
 };
+
+// husky hook test;

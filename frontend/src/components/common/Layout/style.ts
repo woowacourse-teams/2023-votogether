1c25775d3da46d05fe1b1d6b1c6741e6a5934a8e
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2023-07-20 18:08:34 +0900

    레이아웃 컴포넌트 구현 (#87)
    * feat: (#62) 레이아웃 컴포넌트 구현
    
    * refactor: (#62) 사이드바를 숨기는 여부의 props 변수명 변경

---
+++ frontend/src/components/common/Layout/style.ts
@@ -0,0 +1,55 @@
+import { styled } from 'styled-components';
+
+import { theme } from '@styles/theme';
+
+export const Container = styled.div`
+  height: 100vh;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+
+  @media (min-width: ${theme.breakpoint.sm}) {
+    padding-top: 70px;
+  }
+`;
+
+export const WideHeaderWrapper = styled.div`
+  width: 100%;
+
+  position: fixed;
+  top: 0;
+
+  @media (max-width: ${theme.breakpoint.sm}) {
+    display: none;
+    visibility: hidden;
+  }
+`;
+
+export const DashboardWrapper = styled.aside`
+  height: 90vh;
+
+  position: fixed;
+  left: 0;
+
+  @media (max-width: ${theme.breakpoint.sm}) {
+    display: none;
+    visibility: hidden;
+  }
+`;
+
+export const MainContainer = styled.main<{ $isSidebarVisible: boolean }>`
+  display: flex;
+  justify-content: center;
+
+  width: 100%;
+  @media (min-width: ${theme.breakpoint.sm}) {
+    padding-left: ${({ $isSidebarVisible }) => $isSidebarVisible && '225px'};
+  }
+`;
+
+export const ChildrenWrapper = styled.div<{ $isSidebarVisible: boolean }>`
+  width: 100%;
+  max-width: ${({ $isSidebarVisible }) => $isSidebarVisible && '500px'};
+`;

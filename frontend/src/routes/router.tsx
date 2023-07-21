e781257c9a99fd4f6d4980f469e6cf740fa5ca64
inyeong-kang <tomatorkd@naver.com>
2023-07-21 13:23:02 +0900

    feat: (#74) 페이지 라우팅 구현

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -1,5 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import Login from '@pages/auth/Login';
+import Redirection from '@pages/auth/Redirection';
 import Home from '@pages/Home';
 import CreatePost from '@pages/post/CreatePost';
 import EditPost from '@pages/post/EditPost';
@@ -26,6 +28,14 @@ const router = createBrowserRouter([
       {
         path: 'posts/result/:postId',
         element: <VoteStatisticsPage />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'auth/kakao/callback',
+        element: <Redirection />,
       },
     ],
   },

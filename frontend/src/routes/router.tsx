b2e904f9f7c844d277ff2a2c86a37f9cb46c5a7e
chsua <csj1919@naver.com>
2023-07-25 18:31:00 +0900

    feat: (#123) 라우팅 경로 추가

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -12,21 +12,47 @@ const router = createBrowserRouter([
     children: [
       { path: '', element: <Home /> },
       {
-        path: 'posts/write',
-        element: <CreatePost />,
+        path: 'posts/',
+        children: [
+          {
+            path: 'write',
+            element: <CreatePost />,
+          }, //글작성 페이지
+          {
+            path: 'write/:postId',
+            element: <EditPost />,
+          }, //글수정 페이지
+          {
+            path: ':postId',
+            element: <PostDetailPage />,
+          }, //글 상세 페이지
+          {
+            path: 'result/:postId',
+            element: <VoteStatisticsPage />,
+          }, //글 통계 페이지
+        ],
       },
-      {
-        path: 'posts/:postId',
-        element: <PostDetailPage />,
-      },
-      {
-        path: 'posts/write/:postId',
-        element: <EditPost />,
-      },
-      {
-        path: 'posts/result/:postId',
-        element: <VoteStatisticsPage />,
-      },
+      // { path: 'login/', element: <Home /> },
+      // { path: 'landing/', element: <Home /> },
+      // { path: 'admin/', element: <Home /> },
+      { path: 'search?keyword=:category', element: <Home /> },
+      // {
+      //   path: 'users/',
+      //   children: [
+      //     {
+      //       path: 'posts',
+      //       element: <CreatePost />,
+      //     }, //작성한 글 목록 페이지
+      //     {
+      //       path: 'votes',
+      //       element: <EditPost />,
+      //     }, //투표한 글 목록 페이지
+      //     {
+      //       path: 'myPage',
+      //       element: <PostDetailPage />,
+      //     }, //유저 정보 페이지
+      //   ],
+      // },
     ],
   },
 ]);

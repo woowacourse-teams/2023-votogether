e7835a6018de4e404871581a3e968685c94b1f43
chsua <csj1919@naver.com>
2023-07-25 18:39:38 +0900

    refactor: (#123) 레이아웃 컴포넌트와 동일 기능을 하는 스타일드 컴포넌트 삭제

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,34 +1,24 @@
 import Layout from '@components/common/Layout';
-import PostList from '@components/post/PostList';
 import PostListPage from '@components/post/PostListPage';
 
 import { MOCK_FAVORITE_CATEGORIES } from '@mocks/mockData/category';
 import { MOCK_USER_INFO } from '@mocks/mockData/user';
 
-import * as S from './style';
-
 export default function Home() {
   return (
-    <>
-      <S.Web>
-        <Layout
-          userInfo={MOCK_USER_INFO}
-          categoryList={MOCK_FAVORITE_CATEGORIES}
-          isSidebarVisible={true}
-          handleFavoriteClick={() => {}}
-          handleLogoutClick={() => {}}
-        >
-          <PostList />
-        </Layout>
-      </S.Web>
-      <S.Mobile>
-        <PostListPage
-          userInfo={MOCK_USER_INFO}
-          categoryList={MOCK_FAVORITE_CATEGORIES}
-          handleFavoriteClick={() => {}}
-          handleLogoutClick={() => {}}
-        />
-      </S.Mobile>
-    </>
+    <Layout
+      userInfo={MOCK_USER_INFO}
+      categoryList={MOCK_FAVORITE_CATEGORIES}
+      isSidebarVisible={true}
+      handleFavoriteClick={() => {}}
+      handleLogoutClick={() => {}}
+    >
+      <PostListPage
+        userInfo={MOCK_USER_INFO}
+        categoryList={MOCK_FAVORITE_CATEGORIES}
+        handleFavoriteClick={() => {}}
+        handleLogoutClick={() => {}}
+      />
+    </Layout>
   );
 }

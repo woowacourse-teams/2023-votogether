cbf29d3040b27cfd98858af5343b54e747e5a542
inyeong-kang <tomatorkd@naver.com>
2023-07-26 23:31:35 +0900

    chore: (#92) husky pre-push 동작하는지 테스트 2

--- frontend/__test__/hooks/query/usePostList.test.tsx
+++ frontend/__test__/hooks/query/usePostList.test.tsx
@@ -15,8 +15,8 @@ const wrapper = ({ children }: { children: ReactNode }) => (
 );
 */
 
+/*
 describe('usePostList 훅이 게시글 목록을 불러오는지 확인한다.', () => {
-  /*
   test('전체 게시글 목록을 불러온다.', async () => {
     const { result } = renderHook(
       () => usePostList({ postSorting: 'popular', postStatus: 'all' }),
@@ -38,5 +38,5 @@ describe('usePostList 훅이 게시글 목록을 불러오는지 확인한다.',
 
     await waitFor(() => expect(result.current.data?.pages[0].postList).toEqual(MOCK_POST_LIST[0]));
   });
-  */
 });
+*/

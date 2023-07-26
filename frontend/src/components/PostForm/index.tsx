cfab3123f1ad9064da2baf59054e55cba5bc4329
chsua <csj1919@naver.com>
2023-07-26 20:17:00 +0900

    style: (#145) console.log 삭제

--- frontend/src/components/PostForm/index.tsx
+++ frontend/src/components/PostForm/index.tsx
@@ -42,7 +42,6 @@ export default function PostForm({ data, mutate, isError, error }: PostFormProps
 
   const navigate = useNavigate();
 
-  console.log(options);
   const { isOpen, openComponent, closeComponent } = useToggle();
   const [time, setTime] = useState({
     day: 0,

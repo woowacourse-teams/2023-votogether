ade49b5ce8d195f61a1f5bc6d3531c18bad27b11
gilpop8663 <wolfye@naver.com>
2023-07-27 21:43:55 +0900

    refactor: (#127) path 상수에서 뒤에 아이디가 붙은 것을 제거 path 상수를 이용하기 좋도록 수정하였음, 아이디가 붙으면 상수를 이용하는 곳이 없을 것이라고 생각함 post의 path로 정보를 확인하는 훅에서 상수 사용

--- frontend/src/hooks/usePostRequestInfo.ts
+++ frontend/src/hooks/usePostRequestInfo.ts
@@ -2,15 +2,16 @@ import { useLocation, useParams } from 'react-router-dom';
 
 import { PostRequestKind } from '@components/post/PostListPage/types';
 
+import { PATH } from '@constants/path';
 import { POST_CONTENT } from '@constants/post';
 
 import { getPathFragment } from '@utils/getPathFragment';
 
 const REQUEST_URL: Record<string, PostRequestKind> = {
-  '/': POST_CONTENT.ALL,
-  '/posts/category': POST_CONTENT.CATEGORY,
-  '/users/posts': POST_CONTENT.MY_POST,
-  '/users/votes': POST_CONTENT.MY_VOTE,
+  [PATH.HOME]: POST_CONTENT.ALL,
+  [PATH.POST_CATEGORY]: POST_CONTENT.CATEGORY,
+  [PATH.USER_POST]: POST_CONTENT.MY_POST,
+  [PATH.USER_VOTE]: POST_CONTENT.MY_VOTE,
 };
 
 export const usePostRequestInfo = () => {

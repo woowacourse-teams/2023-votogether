c8f173c3d6fafb591ebc10e88409cbab3798ce05
woo-chang <clllickme@naver.com>
2023-07-27 12:43:15 +0900

    feat: (#128) Swagger Auth 설정 추가

--- backend/src/main/java/com/votogether/config/OpenAPIConfig.java
+++ backend/src/main/java/com/votogether/config/OpenAPIConfig.java
@@ -1,7 +1,12 @@
 package com.votogether.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityScheme.In;
+import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import io.swagger.v3.oas.models.servers.Server;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,9 +42,22 @@ public class OpenAPIConfig {
                 .version("v1.0.0")
                 .description("보투게더 API");
 
+        final SecurityScheme securityScheme = new SecurityScheme()
+                .type(Type.HTTP)
+                .in(In.HEADER)
+                .name("Authorization")
+                .scheme("bearer")
+                .bearerFormat("JWT")
+                .description("Bearer JWT");
+
+        final SecurityRequirement securityRequirement = new SecurityRequirement()
+                .addList("bearerAuth");
+
         return new OpenAPI()
                 .info(info)
-                .servers(List.of(devServer, prodServer));
+                .servers(List.of(devServer, prodServer))
+                .components(new Components().addSecuritySchemes("bearerAuth", securityScheme))
+                .security(List.of(securityRequirement));
     }
 
 }

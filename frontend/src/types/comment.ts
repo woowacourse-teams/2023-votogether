8187408e6292766fc192bf2bd8ce92b2a1f5e04e
Gilpop8663 <wolfye@naver.com>
2023-07-27 13:21:59 +0900

    feat: (#152) 댓글 타입 선언

---
+++ frontend/src/types/comment.ts
@@ -0,0 +1,21 @@
+export interface Comment {
+  id: number;
+  member: {
+    id: number;
+    nickname: string;
+  };
+  content: string;
+  createdAt: string;
+  isEdit: boolean;
+}
+
+export interface CommentResponse {
+  id: number;
+  member: {
+    id: number;
+    nickname: string;
+  };
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+}

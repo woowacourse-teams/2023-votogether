e24536e2743b29306d2649e6b671ce3ca0c98c71
jeomxon <jeomxon@gmail.com>
2023-07-27 11:11:08 +0900

    refactor: (#81) 회원 정보 조회 검증에서 불필요한 코드 제거

--- backend/src/test/java/com/votogether/domain/member/controller/MemberControllerTest.java
+++ backend/src/test/java/com/votogether/domain/member/controller/MemberControllerTest.java
@@ -6,9 +6,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
 import com.votogether.domain.member.dto.MemberInfoResponse;
-import com.votogether.domain.member.entity.Gender;
 import com.votogether.domain.member.entity.Member;
-import com.votogether.domain.member.entity.SocialType;
 import com.votogether.domain.member.service.MemberService;
 import com.votogether.global.jwt.TokenProcessor;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
@@ -19,7 +17,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
-import org.springframework.http.HttpHeaders;
 
 @Import(TokenProcessor.class)
 @WebMvcTest(MemberController.class)
@@ -40,16 +37,6 @@ class MemberControllerTest {
     @DisplayName("회원 정보를 조회한다.")
     void findMemberInfo() {
         // given
-        Member member = Member.builder()
-                .nickname("저문")
-                .gender(Gender.MALE)
-                .ageRange("20~29")
-                .birthday("0101")
-                .socialType(SocialType.KAKAO)
-                .socialId("123123")
-                .point(1234)
-                .build();
-
         MemberInfoResponse memberInfoResponse = new MemberInfoResponse(
                 "저문",
                 1234,
@@ -57,16 +44,11 @@ class MemberControllerTest {
                 0
         );
 
-        given(memberService.register(member)).willReturn(member);
-        Member registeredMember = memberService.register(member);
-        String token = tokenProcessor.generateToken(registeredMember);
-
         given(memberService.findMemberInfo(any(Member.class))).willReturn(memberInfoResponse);
 
         // when
         MemberInfoResponse response = RestAssuredMockMvc
                 .given().log().all()
-                .headers(HttpHeaders.AUTHORIZATION, "Bearer " + token)
                 .when().get("/members/me")
                 .then().log().all()
                 .extract()

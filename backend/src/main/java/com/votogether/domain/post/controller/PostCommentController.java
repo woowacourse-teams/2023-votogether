84a9e3100d677547c6578a80695f8b932931b632
woo-chang <clllickme@naver.com>
2023-07-27 15:12:55 +0900

    feat: (#128) Swagger 문서화

--- backend/src/main/java/com/votogether/domain/post/controller/PostCommentController.java
+++ backend/src/main/java/com/votogether/domain/post/controller/PostCommentController.java
@@ -3,8 +3,12 @@ package com.votogether.domain.post.controller;
 import com.votogether.domain.member.entity.Member;
 import com.votogether.domain.post.dto.request.CommentRegisterRequest;
 import com.votogether.domain.post.service.PostCommentService;
+import com.votogether.exception.ExceptionResponse;
 import com.votogether.global.jwt.Auth;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -29,12 +33,16 @@ public class PostCommentController {
     @Operation(summary = "게시글 댓글 작성", description = "게시글 댓글을 작성한다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "게시글 댓글 작성 성공"),
-            @ApiResponse(responseCode = "404", description = "존재하지 않는 게시글에 대한 댓글 작성")
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "존재하지 않는 게시글에 대한 댓글 작성",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
+            )
     })
     @PostMapping("/{postId}/comments")
     public ResponseEntity<Void> registerComment(
             @Auth final Member member,
-            @PathVariable final Long postId,
+            @PathVariable @Parameter(description = "댓글 작성 게시글 ID") final Long postId,
             @Valid @RequestBody CommentRegisterRequest commentRegisterRequest
     ) {
         postCommentService.registerComment(member, postId, commentRegisterRequest);

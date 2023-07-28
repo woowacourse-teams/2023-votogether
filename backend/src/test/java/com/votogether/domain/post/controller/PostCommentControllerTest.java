57fac6d74f08cb805205c5347354d57c2237910b
최우창 <clllickme@naver.com>
2023-07-28 14:04:55 +0900

    댓글 작성 기능 구현 (#151)
    * feat: (#128) 댓글 도메인 구현
    
    * feat: (#128) 댓글 등록 기능 구현
    
    * feat: (#128) 게시글 댓글 작성 API 구현
    
    * feat: (#128) Swagger Auth 설정 추가
    
    * feat: (#128) Swagger 문서화
    
    * feat: (#128) Swagger 환경에 따른 설정 분리
    
    * refactor: (#128) 사용하지 않는 의존성 삭제
    
    * refactor: (#128) 메서드에 사용되는 객체 추출
    
    * fix: (#128) 테스트에서 사용하지 않는 설정 삭제
    
    * refactor: (#128) 명확한 의미를 가진 메서드명으로 수정
    
    * refactor: (#128) 게시글이 존재하지 않을 때 예외 수정
    
    * refactor: (#128) applicaion/json 기본 설정 제거
    
    * refactor: (#128) 커스텀 예외 처리 방식 수정
    
    * feat: (#128) 테스트 쿼리 확인 설정 추가

---
+++ backend/src/test/java/com/votogether/domain/post/controller/PostCommentControllerTest.java
@@ -0,0 +1,117 @@
+package com.votogether.domain.post.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.BDDMockito.given;
+
+import com.votogether.domain.member.service.MemberService;
+import com.votogether.domain.post.dto.request.CommentRegisterRequest;
+import com.votogether.domain.post.service.PostCommentService;
+import com.votogether.fixtures.MemberFixtures;
+import com.votogether.global.jwt.TokenPayload;
+import com.votogether.global.jwt.TokenProcessor;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.context.WebApplicationContext;
+
+@WebMvcTest(PostCommentController.class)
+class PostCommentControllerTest {
+
+    @MockBean
+    PostCommentService postCommentService;
+
+    @MockBean
+    MemberService memberService;
+
+    @MockBean
+    TokenProcessor tokenProcessor;
+
+    @BeforeEach
+    void setUp(final WebApplicationContext webApplicationContext) {
+        RestAssuredMockMvc.standaloneSetup(new PostCommentController(postCommentService));
+        RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
+    }
+
+    @Nested
+    @DisplayName("게시글 댓글 등록 시 ")
+    class CreateComment {
+
+        @ParameterizedTest
+        @ValueSource(strings = {"@", "a", "가"})
+        @DisplayName("ID로 변환할 수 없는 타입이라면 400을 응답한다.")
+        void invalidIDType(String id) throws Exception {
+            // given
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(MemberFixtures.MALE_20.get());
+
+            // when, then
+            RestAssuredMockMvc.given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .when().post("/posts/{postId}/comments", id)
+                    .then().log().all()
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("code", equalTo(-9998))
+                    .body("message", equalTo("postId는 Long 타입이 필요합니다."));
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @DisplayName("댓글 내용이 존재하지 않으면 400을 응답한다.")
+        void emptyContent(final String content) throws Exception {
+            // given
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(MemberFixtures.MALE_20.get());
+            CommentRegisterRequest commentRegisterRequest = new CommentRegisterRequest(content);
+
+            // when, then
+            RestAssuredMockMvc.given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(commentRegisterRequest)
+                    .when().post("/posts/{postId}/comments", 1)
+                    .then().log().all()
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body("code", equalTo(-9997))
+                    .body("message", containsString("댓글 내용은 존재해야 합니다."));
+        }
+
+        @Test
+        @DisplayName("댓글을 정상적으로 등록하면 201을 응답한다.")
+        void createComment() throws Exception {
+            // given
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(MemberFixtures.MALE_20.get());
+            CommentRegisterRequest commentRegisterRequest = new CommentRegisterRequest("댓글입니다.");
+
+            // when, then
+            RestAssuredMockMvc.given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .body(commentRegisterRequest)
+                    .when().post("/posts/{postId}/comments", 1)
+                    .then().log().all()
+                    .status(HttpStatus.CREATED);
+        }
+
+    }
+
+}
---
+++ backend/src/test/java/com/votogether/domain/post/service/PostCommentServiceTest.java
@@ -0,0 +1,77 @@
+package com.votogether.domain.post.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import com.votogether.ServiceTest;
+import com.votogether.domain.member.entity.Member;
+import com.votogether.domain.member.repository.MemberRepository;
+import com.votogether.domain.post.dto.request.CommentRegisterRequest;
+import com.votogether.domain.post.entity.Post;
+import com.votogether.domain.post.entity.PostBody;
+import com.votogether.domain.post.repository.CommentRepository;
+import com.votogether.domain.post.repository.PostRepository;
+import com.votogether.exception.NotFoundException;
+import com.votogether.fixtures.MemberFixtures;
+import java.time.LocalDateTime;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@ServiceTest
+class PostCommentServiceTest {
+
+    @Autowired
+    PostCommentService postCommentService;
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Autowired
+    PostRepository postRepository;
+
+    @Autowired
+    CommentRepository commentRepository;
+
+    @Nested
+    @DisplayName("게시글 댓글 등록")
+    class CreateComment {
+
+        @Test
+        @DisplayName("존재하지 않는 게시글이라면 예외를 던진다.")
+        void emptyPost() {
+            // given
+            Member member = memberRepository.save(MemberFixtures.MALE_20.get());
+            CommentRegisterRequest commentRegisterRequest = new CommentRegisterRequest("hello");
+
+            // when, then
+            assertThatThrownBy(() -> postCommentService.createComment(member, -1L, commentRegisterRequest))
+                    .isInstanceOf(NotFoundException.class)
+                    .hasMessage("해당 게시글이 존재하지 않습니다.");
+        }
+
+        @Test
+        @DisplayName("게시글에 댓글을 등록한다.")
+        void createComment() {
+            // given
+            Member member = memberRepository.save(MemberFixtures.MALE_20.get());
+            Post post = postRepository.save(
+                    Post.builder()
+                            .member(member)
+                            .postBody(PostBody.builder().title("title").content("content").build())
+                            .deadline(LocalDateTime.of(2100, 7, 12, 0, 0))
+                            .build()
+            );
+            CommentRegisterRequest commentRegisterRequest = new CommentRegisterRequest("hello");
+
+            // when
+            postCommentService.createComment(member, post.getId(), commentRegisterRequest);
+
+            // then
+            assertThat(commentRepository.findAll()).hasSize(1);
+        }
+
+    }
+
+}

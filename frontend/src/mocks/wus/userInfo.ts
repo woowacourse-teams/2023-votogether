92e5375fd3e2d2f03b48a09b0c824395385f9a95
Gilpop8663 <wolfye@naver.com>
2023-07-31 17:48:21 +0900

    feat: (#153): 유저 닉네임 변경, 회원 탈퇴 MSW 코드 작성

--- frontend/src/mocks/wus/userInfo.ts
+++ frontend/src/mocks/wus/userInfo.ts
@@ -6,4 +6,16 @@ export const mockUserInfo = [
   rest.get('/members/me', (req, res, ctx) => {
     return res(ctx.status(200), ctx.json(MOCK_USER_INFO));
   }),
+
+  rest.patch('/members/me/nickname', (req, res, ctx) => {
+    MOCK_USER_INFO.nickname = 'wood';
+
+    return res(ctx.status(200), ctx.json({ ok: true }));
+  }),
+
+  rest.delete('/members/me/delete', (req, res, ctx) => {
+    MOCK_USER_INFO.nickname = 'cancel';
+
+    return res(ctx.status(204));
+  }),
 ];

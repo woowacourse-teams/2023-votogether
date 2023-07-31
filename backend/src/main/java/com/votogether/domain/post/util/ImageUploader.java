4467d878629d47dfc6ae9c8d70dabc4ebc81a7b2
JunH <tjdtls690@gmail.com>
2023-07-31 16:11:15 +0900

    refactor: (#172) 이미지 파일 이름 중복을 방지하기 위해 밀리초를 이름에 포함하는 것으로 개선

--- backend/src/main/java/com/votogether/domain/post/util/ImageUploader.java
+++ backend/src/main/java/com/votogether/domain/post/util/ImageUploader.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,8 +14,10 @@ import org.springframework.web.multipart.MultipartFile;
 public class ImageUploader {
 
     public static String upload(final MultipartFile image) {
+        final long milli = LocalDateTime.now().toInstant(ZoneOffset.UTC).toEpochMilli();
+
         final String absolutePath = new File("").getAbsolutePath();
-        final String imageUrl = absolutePath + "/images/" + image.getOriginalFilename();
+        final String imageUrl = absolutePath + "/images/" + milli + "_" + image.getOriginalFilename();
 
         try {
             Files.write(Paths.get(imageUrl), image.getBytes());

052b62421ce7cdcbeec0c6074027c755511de54f
JunH <tjdtls690@gmail.com>
2023-08-01 14:24:05 +0900

    refactor: (#172) 메서드를 사용하는 클래스 변경

--- backend/src/main/java/com/votogether/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/votogether/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -42,7 +43,7 @@ public class GlobalExceptionHandler {
         final List<String> errorMessages = e.getBindingResult()
                 .getAllErrors()
                 .stream()
-                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .map(ObjectError::getDefaultMessage)
                 .toList();
         log.warn("[" + e.getClass() + "] : " + errorMessages);
         return ResponseEntity.badRequest()

a7586ad89a205032e1d94c0a9e215b739d057f43
gilpop8663 <wolfye@naver.com>
2023-08-02 23:50:37 +0900

    refactor: (#205) api 폴더에 있던 wus 폴더 삭제 및 파일 이동

--- frontend/src/hooks/query/user/useUserInfo.ts
+++ frontend/src/hooks/query/user/useUserInfo.ts
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 
 import { UserInfoResponse } from '@type/user';
 
-import { getUserInfo } from '@api/wus/userInfo';
+import { getUserInfo } from '@api/userInfo';
 
 import { QUERY_KEY } from '@constants/queryKey';
 

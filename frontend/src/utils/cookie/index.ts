e067c3c27d4c24dca8734ed46e9ddcb0cce6cdc4
chsua <csj1919@naver.com>
2023-08-02 16:16:47 +0900

    fix: (#74) 쿠키가 브라우저에 저장 안되는 오류 수정
    - path=/ 를 통해 모든 url에서 쿠키 접근가능하도록 수정

--- frontend/src/utils/cookie/index.ts
+++ frontend/src/utils/cookie/index.ts
@@ -2,7 +2,7 @@ type CookieKey = 'accessToken' | 'refreshToken';
 
 export const setCookieToken = (key: CookieKey, token: string) => {
   //secure 속성은 현재 dev에서는 http로 진행중이기 때문에 사용할 수 없음
-  document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(token)}; `;
+  document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(token)}; path=/`;
 };
 
 // token형식 = "key=value; key=value; key=value"

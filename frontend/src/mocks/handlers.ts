89ec68cb1586990cb09029383165c3996965226e
gilpop8663 <wolfye@naver.com>
2023-08-02 23:46:57 +0900

    refactor: (#205) wus 폴더에 있던 유저 MSW 코드 밖으로 이동

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -3,9 +3,9 @@ import { mockComment } from './comment';
 import { example } from './example/get';
 import { mockVoteResult } from './getVoteDetail';
 import { mockPost } from './post';
+import { mockUserInfo } from './userInfo';
 import { mockVote } from './vote';
 import { mockPostList } from './wus/post';
-import { mockUserInfo } from './wus/userInfo';
 
 export const handlers = [
   ...example,

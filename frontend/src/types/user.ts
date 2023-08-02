8c1e629c995be1ee6520df99f57926b8a20f0a89
chsua <csj1919@naver.com>
2023-08-02 09:56:05 +0900

    refactor: (#74) 로그인 정보 변수 타입 위치 이동

--- frontend/src/types/user.ts
+++ frontend/src/types/user.ts
@@ -17,3 +17,9 @@ export interface UserInfoResponse {
 export interface ModifyNicknameRequest {
   nickname: string;
 }
+
+export interface LoggedInfo {
+  accessToken: string;
+  isLogin: boolean;
+  userInfo?: UserInfoResponse;
+}

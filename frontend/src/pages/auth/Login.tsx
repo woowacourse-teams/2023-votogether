f7ff0c998c9b6497a74a5f16537f05a4aeae1e6e
chsua <csj1919@naver.com>
2023-08-02 17:10:01 +0900

    feat: (#74)  .env url 규칙에 따라 수정

--- frontend/src/pages/auth/Login.tsx
+++ frontend/src/pages/auth/Login.tsx
@@ -9,8 +9,8 @@ import kakaoLogin from '@assets/kakao_login_medium_wide.svg';
 
 export default function Login() {
   const navigate = useNavigate();
-  const CLIENT_ID = `${process.env.REACT_APP_REST_API_KEY}`;
-  const REDIRECT_URI = `${process.env.REACT_APP_CLIENT_REDIRECT_URL}`;
+  const CLIENT_ID = `${process.env.VOTOGETHER_REST_API_KEY}`;
+  const REDIRECT_URI = `${process.env.VOTOGETHER_CLIENT_REDIRECT_URL}`;
   const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
   return (

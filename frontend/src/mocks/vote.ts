73b7151ede37517f1761a3a3d8f08e8194a4503e
Gilpop8663 <wolfye@naver.com>
2023-08-03 14:45:27 +0900

    fix: (#220) 처음 투표를 할 때 사용하는 커스텀 훅 구현

--- frontend/src/mocks/vote.ts
+++ frontend/src/mocks/vote.ts
@@ -1,8 +1,12 @@
 import { rest } from 'msw';
 
+import { MOCK_POST_INFO } from './mockData/post';
+
 export const mockVote = [
   //투표
   rest.post(`/posts/:postId/options/:optionId`, (req, res, ctx) => {
+    MOCK_POST_INFO.voteInfo.selectedOptionId = 999;
+
     return res(ctx.status(200), ctx.json({ message: 'ok' }));
   }),
 

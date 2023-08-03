015733ae642c503ddd66a032d4fbe4df0cef2999
최우창 <clllickme@naver.com>
2023-08-04 00:51:45 +0900

    fix: (#245) 조기 마감 제한 삭제 (#246)

--- backend/src/main/java/com/votogether/domain/post/service/PostService.java
+++ backend/src/main/java/com/votogether/domain/post/service/PostService.java
@@ -236,14 +236,13 @@ public class PostService {
         return ageRange;
     }
 
-    @Transactional(readOnly = true)
+    @Transactional
     public void closePostEarlyById(final Long id, final Member loginMember) {
         final Post post = postRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글은 존재하지 않습니다."));
 
         post.validateWriter(loginMember);
         post.validateDeadLine();
-        post.validateHalfDeadLine();
         post.closeEarly();
     }
 

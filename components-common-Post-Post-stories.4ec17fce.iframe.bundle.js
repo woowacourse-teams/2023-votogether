851e9eb44c490bf17ac00c6a7c59e68ef9d36d66
inyeong-kang <inyeong-kang@users.noreply.github.com>
2023-08-03 05:07:16 +0000

    deploy: b4e3c2255545fe027cc9bfcaec2185ab3f6499d9

---
+++ components-common-Post-Post-stories.4ec17fce.iframe.bundle.js
@@ -0,0 +1,177 @@
+"use strict";(self.webpackChunkvotogether=self.webpackChunkvotogether||[]).push([[2112],{"./src/components/common/Post/Post.stories.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.r(__webpack_exports__),__webpack_require__.d(__webpack_exports__,{NotVotedPost:()=>NotVotedPost,PreviewNotVotedPost:()=>PreviewNotVotedPost,PreviewVotedPost:()=>PreviewVotedPost,VotedPost:()=>VotedPost,__namedExportsOrder:()=>__namedExportsOrder,default:()=>__WEBPACK_DEFAULT_EXPORT__});var _mockData__WEBPACK_IMPORTED_MODULE_0__=__webpack_require__("./src/components/common/Post/mockData.ts"),___WEBPACK_IMPORTED_MODULE_1__=__webpack_require__("./src/components/common/Post/index.tsx"),react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__=__webpack_require__("./node_modules/react/jsx-runtime.js");const __WEBPACK_DEFAULT_EXPORT__={component:___WEBPACK_IMPORTED_MODULE_1__.Z,decorators:[storyFn=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)("div",{style:{width:"576px"},children:storyFn()})]},PreviewNotVotedPost={render:()=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(___WEBPACK_IMPORTED_MODULE_1__.Z,{postInfo:_mockData__WEBPACK_IMPORTED_MODULE_0__.V,isPreview:!0})},PreviewVotedPost={render:()=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(___WEBPACK_IMPORTED_MODULE_1__.Z,{postInfo:_mockData__WEBPACK_IMPORTED_MODULE_0__.M,isPreview:!0})},NotVotedPost={render:()=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(___WEBPACK_IMPORTED_MODULE_1__.Z,{postInfo:_mockData__WEBPACK_IMPORTED_MODULE_0__.V,isPreview:!1})},VotedPost={render:()=>(0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_2__.jsx)(___WEBPACK_IMPORTED_MODULE_1__.Z,{postInfo:_mockData__WEBPACK_IMPORTED_MODULE_0__.M,isPreview:!1})};PreviewNotVotedPost.parameters={...PreviewNotVotedPost.parameters,docs:{...PreviewNotVotedPost.parameters?.docs,source:{originalSource:"{\n  render: () => <Post postInfo={MOCK_NOT_VOTE_POST} isPreview={true} />\n}",...PreviewNotVotedPost.parameters?.docs?.source}}},PreviewVotedPost.parameters={...PreviewVotedPost.parameters,docs:{...PreviewVotedPost.parameters?.docs,source:{originalSource:"{\n  render: () => <Post postInfo={MOCK_VOTE_POST} isPreview={true} />\n}",...PreviewVotedPost.parameters?.docs?.source}}},NotVotedPost.parameters={...NotVotedPost.parameters,docs:{...NotVotedPost.parameters?.docs,source:{originalSource:"{\n  render: () => <Post postInfo={MOCK_NOT_VOTE_POST} isPreview={false} />\n}",...NotVotedPost.parameters?.docs?.source}}},VotedPost.parameters={...VotedPost.parameters,docs:{...VotedPost.parameters?.docs,source:{originalSource:"{\n  render: () => <Post postInfo={MOCK_VOTE_POST} isPreview={false} />\n}",...VotedPost.parameters?.docs?.source}}};const __namedExportsOrder=["PreviewNotVotedPost","PreviewVotedPost","NotVotedPost","VotedPost"]},"./src/components/common/Post/index.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>Post});var post=__webpack_require__("./src/api/post.ts"),WrittenVoteOptionList=__webpack_require__("./src/components/optionList/WrittenVoteOptionList/index.tsx"),path=__webpack_require__("./src/constants/path.ts"),vote=__webpack_require__("./src/constants/vote.ts"),dist=__webpack_require__("./node_modules/react-router-dom/dist/index.js"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),theme=__webpack_require__("./src/styles/theme.ts");const Container=styled_components_browser_esm.zo.li`
+  font: var(--text-small);
+  letter-spacing: 0.5px;
+  line-height: 1.5;
+
+  @media (min-width: ${theme.r.breakpoint.sm}) {
+    font: var(--text-caption);
+  }
+`,Category=styled_components_browser_esm.zo.span`
+  font: var(--text-small);
+
+  @media (min-width: ${theme.r.breakpoint.sm}) {
+    font: var(--text-caption);
+  }
+`,Title=styled_components_browser_esm.zo.p`
+  display: -webkit-box;
+
+  font: var(--text-title);
+  text-overflow: ellipsis;
+  word-break: break-word;
+
+  overflow: hidden;
+
+  -webkit-line-clamp: ${props=>props.$isPreview&&"2"};
+  -webkit-box-orient: vertical;
+
+  @media (min-width: ${theme.r.breakpoint.sm}) {
+    font-size: 2.2rem;
+  }
+`,Wrapper=styled_components_browser_esm.zo.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  font: var(--text-small);
+
+  & > :nth-child(2) {
+    margin-left: 10px;
+  }
+
+  @media (min-width: ${theme.r.breakpoint.sm}) {
+    font: var(--text-caption);
+  }
+`,Content=styled_components_browser_esm.zo.p`
+  display: -webkit-box;
+
+  font: var(--text-caption);
+  text-overflow: ellipsis;
+  word-break: break-word;
+
+  margin: 10px 0;
+
+  overflow: hidden;
+
+  -webkit-line-clamp: ${props=>props.$isPreview&&"10"};
+  -webkit-box-orient: vertical;
+
+  @media (min-width: ${theme.r.breakpoint.sm}) {
+    font: var(--text-body);
+  }
+`,DetailLink=(0,styled_components_browser_esm.zo)(dist.rU)`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+
+  pointer-events: ${({$isPreview})=>!$isPreview&&"none"};
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function Post({postInfo,isPreview}){const{postId,category,title,writer,createTime,deadline,content,voteInfo}=postInfo;return(0,jsx_runtime.jsxs)(Container,{children:[(0,jsx_runtime.jsxs)(DetailLink,{to:isPreview?`${path.m.POST}/${postId}`:"#",$isPreview:isPreview,onClick:e=>{isPreview||e.preventDefault()},"aria-describedby":isPreview?"해당 게시물의 상세페이지로 이동하기":"현재 상세페이지이므로 사용할 수 없음","aria-disabled":!isPreview,children:[(0,jsx_runtime.jsx)(Category,{"aria-label":"카테고리",children:category.map((category=>category.name)).join(" | ")}),(0,jsx_runtime.jsx)(Title,{"aria-label":"제목",$isPreview:isPreview,children:title}),(0,jsx_runtime.jsxs)(Wrapper,{children:[(0,jsx_runtime.jsx)("span",{"aria-label":"작성자",children:writer.nickname}),(0,jsx_runtime.jsxs)(Wrapper,{children:[(0,jsx_runtime.jsx)("span",{"aria-label":"작성일시",children:startTime}),(0,jsx_runtime.jsx)("span",{"aria-label":"투표 마감일시",children:endTime})]})]}),(0,jsx_runtime.jsx)(Content,{"aria-label":"내용",$isPreview:isPreview,children:content})]}),(0,jsx_runtime.jsx)(WrittenVoteOptionList.Z,{selectedOptionId:voteInfo.selectedOptionId,handleVoteClick:newOptionId=>{voteInfo.selectedOptionId!==newOptionId&&(voteInfo.selectedOptionId!==vote.a.NOT_VOTE?(0,post.hQ)(postId,{originOptionId:voteInfo.selectedOptionId,newOptionId}):(0,post.Gi)(postId,newOptionId))},isPreview,voteOptionList:voteInfo.options})]})}Post.displayName="Post";try{Post.displayName="Post",Post.__docgenInfo={description:"",displayName:"Post",props:{postInfo:{defaultValue:null,description:"",name:"postInfo",required:!0,type:{name:"PostInfo"}},isPreview:{defaultValue:null,description:"",name:"isPreview",required:!0,type:{name:"boolean"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/common/Post/index.tsx#Post"]={docgenInfo:Post.__docgenInfo,name:"Post",path:"src/components/common/Post/index.tsx#Post"})}catch(__react_docgen_typescript_loader_error){}},"./src/components/common/Post/mockData.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{M:()=>MOCK_VOTE_POST,V:()=>MOCK_NOT_VOTE_POST});const MOCK_NOT_VOTE_POST={postId:1111111,title:"어느 곳에서 정보를 찾아야 할지도 막막한 사람들을 위한, 심심풀이로 나의 취향과 남의 취향을 비교해보고 싶은 사람들을 위한 프로젝트",writer:{id:12121221,nickname:"우아한 잔치국수"},content:"이는 사람들에게 재미와 정보, 두 가지를 줄 수 있습니다. 사람들은 MBTI, 밸런스게임처럼 나와 같은 사람들을 찾고, 나와 다른 사람들과 비교하는 것을 즐깁니다. 이를 컨텐츠화하여 보다 빠르게 질문하고 답변하며, 사람들의 반응을 확인할 수 있다면 사람들은 충분한 즐거움을 느낄 것입니다. 또한 20대가 많은 대학가에 창업을 하고 싶지만 20대의 의견을 모르겠을 때, 확실한 답은 아닐지라도 어느 정도의 가이드를 줄 수 있을 것입니다. 질문자에게 제공되는 성별/나이대별 투표 결과 정보는 질문자가 하고자 하는 의사결정의 근거가 될 수 있을 것입니다.",imageUrl:"",category:[{id:76767,name:"개발"},{id:74632,name:"연애"},{id:71347,name:"상담"}],createTime:"2023-07-12 12:40",deadline:"2023-07-20 18:40",voteInfo:{selectedOptionId:0,allPeopleCount:100,options:[{id:12312,text:"당선",peopleCount:-1,percent:-1,imageUrl:""},{id:12314,text:"votogether",peopleCount:-1,percent:-1,imageUrl:""},{id:123152,text:"블라인드와 같은 SNS의 형식을 차용합니다. 누군가는 글을 쓰고, 누군가는 반응합니다. 다만, 댓글은 없습니다. 투표로 자신의 의견을 표현하고 이를 사람들에게 보여줍니다.",peopleCount:-1,percent:-1,imageUrl:""},{id:123122,text:"fun from choice, 오늘도 즐거운 한 표 ",imageUrl:"https://source.unsplash.com/random",peopleCount:-1,percent:-1}]}},MOCK_VOTE_POST={postId:1111112,title:"어느 곳에서 정보를 찾아야 할지도 막막한 사람들을 위한, 심심풀이로 나의 취향과 남의 취향을 비교해보고 싶은 사람들을 위한 프로젝트",writer:{id:12121221,nickname:"우아한 잔치국수"},content:"이는 사람들에게 재미와 정보, 두 가지를 줄 수 있습니다. 사람들은 MBTI, 밸런스게임처럼 나와 같은 사람들을 찾고, 나와 다른 사람들과 비교하는 것을 즐깁니다. 이를 컨텐츠화하여 보다 빠르게 질문하고 답변하며, 사람들의 반응을 확인할 수 있다면 사람들은 충분한 즐거움을 느낄 것입니다. 또한 20대가 많은 대학가에 창업을 하고 싶지만 20대의 의견을 모르겠을 때, 확실한 답은 아닐지라도 어느 정도의 가이드를 줄 수 있을 것입니다. 질문자에게 제공되는 성별/나이대별 투표 결과 정보는 질문자가 하고자 하는 의사결정의 근거가 될 수 있을 것입니다.",imageUrl:"",category:[{id:76767,name:"개발"},{id:74632,name:"연애"},{id:71347,name:"상담"}],createTime:"2023-07-12 12:40",deadline:"2023-07-21 18:40",voteInfo:{selectedOptionId:12312,allPeopleCount:123,options:[{id:12312,text:"당선",peopleCount:30,imageUrl:"",percent:30},{id:12314,text:"votogether",peopleCount:40,imageUrl:"",percent:40},{id:123152,text:"인스타그램, 블라인드와 같은 SNS의 형식을 차용합니다. 누군가는 글을 쓰고, 누군가는 반응합니다. 다만, 댓글은 없습니다. 투표로 자신의 의견을 표현하고 이를 사람들에게 보여줍니다.",peopleCount:20,imageUrl:"",percent:20},{id:123122,text:"fun from choice, 오늘도 즐거운 한 표 ",imageUrl:"https://source.unsplash.com/random",peopleCount:10,percent:10}]}}},"./src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/ProgressBar/index.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>ProgressBar});__webpack_require__("./node_modules/react/index.js");var styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js");const Container=styled_components_browser_esm.zo.div`
+  border-radius: 4px;
+
+  height: 8px;
+
+  background-color: rgba(0, 0, 0, 0.15);
+`,Bar=styled_components_browser_esm.zo.div`
+  border-radius: 4px;
+
+  width: ${({progress})=>`${progress}%`};
+  height: 8px;
+
+  background-color: ${({$isSelected})=>$isSelected?"var(--primary-color)":"#9F9F9F"};
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function ProgressBar({percent,isSelected}){return(0,jsx_runtime.jsx)(Container,{children:(0,jsx_runtime.jsx)(Bar,{progress:percent,$isSelected:isSelected})})}ProgressBar.displayName="ProgressBar";try{ProgressBar.displayName="ProgressBar",ProgressBar.__docgenInfo={description:"",displayName:"ProgressBar",props:{percent:{defaultValue:null,description:"",name:"percent",required:!0,type:{name:"number"}},isSelected:{defaultValue:null,description:"",name:"isSelected",required:!0,type:{name:"boolean"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/ProgressBar/index.tsx#ProgressBar"]={docgenInfo:ProgressBar.__docgenInfo,name:"ProgressBar",path:"src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/ProgressBar/index.tsx#ProgressBar"})}catch(__react_docgen_typescript_loader_error){}},"./src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/index.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>WrittenVoteOption});var ProgressBar=__webpack_require__("./src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/ProgressBar/index.tsx"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),theme=__webpack_require__("./src/styles/theme.ts");const Container=styled_components_browser_esm.zo.button`
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+
+  border: ${({$isSelected})=>$isSelected?"2px solid var(--primary-color)":"1px solid rgba(0, 0, 0, 0.1)"};
+  border-radius: 4px;
+  padding: 10px 15px;
+
+  color: #5b5b5b;
+
+  text-align: left;
+
+  cursor: pointer;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    padding: 15px 25px;
+  }
+`,Image=styled_components_browser_esm.zo.img`
+  border-radius: 4px;
+  margin-bottom: 10px;
+
+  width: 100%;
+
+  aspect-ratio: 1/1;
+  object-fit: cover;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    margin-bottom: 20px;
+  }
+`,PreviewContent=styled_components_browser_esm.zo.p`
+  display: -webkit-box;
+
+  font: var(--text-caption);
+  font-weight: 500;
+  text-overflow: ellipsis;
+  word-break: break-word;
+
+  overflow: hidden;
+
+  -webkit-line-clamp: 2; // 원하는 라인수
+  -webkit-box-orient: vertical;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    font: var(--text-body);
+  }
+`,DetailContent=styled_components_browser_esm.zo.p`
+  font: var(--text-caption);
+  font-weight: 500;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    font: var(--text-body);
+  }
+`,ProgressContainer=styled_components_browser_esm.zo.div`
+  margin-top: 12px;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    margin-top: 18px;
+  }
+`,TextContainer=styled_components_browser_esm.zo.div`
+  margin-top: 8px;
+
+  text-align: end;
+  font-weight: 500;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    margin-top: 12px;
+
+    font: var(--text-body);
+  }
+`,PeopleText=styled_components_browser_esm.zo.span`
+  font: var(--text-caption);
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    font: var(--text-body);
+  }
+`,PercentText=styled_components_browser_esm.zo.span`
+  margin-left: 4px;
+
+  font: var(--text-small);
+
+  opacity: 0.7;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    font: var(--text-caption);
+  }
+`;var jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function WrittenVoteOption({handleVoteClick,text,isVoted,peopleCount,percent,isSelected,isPreview,imageUrl,ariaLabel}){return(0,jsx_runtime.jsxs)(Container,{"aria-label":`${ariaLabel}${isSelected?" 선택된 선택지":""}`,$isSelected:isSelected,onClick:handleVoteClick,children:[!isPreview&&imageUrl&&(0,jsx_runtime.jsx)(Image,{src:imageUrl,alt:text}),isPreview?(0,jsx_runtime.jsx)(PreviewContent,{"aria-label":"선택지 내용",children:text}):(0,jsx_runtime.jsx)(DetailContent,{"aria-label":"선택지 내용",children:text}),isVoted&&(0,jsx_runtime.jsxs)(jsx_runtime.Fragment,{children:[(0,jsx_runtime.jsx)(ProgressContainer,{"aria-label":"",children:(0,jsx_runtime.jsx)(ProgressBar.Z,{percent,isSelected})}),(0,jsx_runtime.jsxs)(TextContainer,{children:[(0,jsx_runtime.jsxs)(PeopleText,{"aria-label":"투표한 인원",children:[peopleCount,"명"]}),(0,jsx_runtime.jsxs)(PercentText,{"aria-label":"전체 투표 중 차지 비율",children:["(",percent.toFixed(1),"%)"]})]})]})]})}WrittenVoteOption.displayName="WrittenVoteOption";try{WrittenVoteOption.displayName="WrittenVoteOption",WrittenVoteOption.__docgenInfo={description:"",displayName:"WrittenVoteOption",props:{handleVoteClick:{defaultValue:null,description:"",name:"handleVoteClick",required:!0,type:{name:"() => void"}},text:{defaultValue:null,description:"",name:"text",required:!0,type:{name:"string"}},isVoted:{defaultValue:null,description:"",name:"isVoted",required:!0,type:{name:"boolean"}},peopleCount:{defaultValue:null,description:"",name:"peopleCount",required:!0,type:{name:"number"}},percent:{defaultValue:null,description:"",name:"percent",required:!0,type:{name:"number"}},isSelected:{defaultValue:null,description:"",name:"isSelected",required:!0,type:{name:"boolean"}},isPreview:{defaultValue:null,description:"",name:"isPreview",required:!0,type:{name:"boolean"}},imageUrl:{defaultValue:null,description:"",name:"imageUrl",required:!0,type:{name:"string"}},ariaLabel:{defaultValue:null,description:"",name:"ariaLabel",required:!0,type:{name:"string"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/index.tsx#WrittenVoteOption"]={docgenInfo:WrittenVoteOption.__docgenInfo,name:"WrittenVoteOption",path:"src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/index.tsx#WrittenVoteOption"})}catch(__react_docgen_typescript_loader_error){}},"./src/components/optionList/WrittenVoteOptionList/index.tsx":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{Z:()=>WrittenVoteOptionList});var vote=__webpack_require__("./src/constants/vote.ts"),styled_components_browser_esm=__webpack_require__("./node_modules/styled-components/dist/styled-components.browser.esm.js"),theme=__webpack_require__("./src/styles/theme.ts");const VoteOptionListContainer=styled_components_browser_esm.zo.div`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+
+  width: 100%;
+
+  @media (min-width: ${theme.r.breakpoint.md}) {
+    gap: 18px;
+  }
+`;var WrittenVoteOption=__webpack_require__("./src/components/optionList/WrittenVoteOptionList/WrittenVoteOption/index.tsx"),jsx_runtime=__webpack_require__("./node_modules/react/jsx-runtime.js");function WrittenVoteOptionList({isPreview,voteOptionList,selectedOptionId,handleVoteClick}){return(0,jsx_runtime.jsx)(VoteOptionListContainer,{"aria-label":"투표 선택지",children:voteOptionList.map(((voteOption,index)=>(0,jsx_runtime.jsx)(WrittenVoteOption.Z,{ariaLabel:`${index+1}번`,...voteOption,isPreview,isVoted:selectedOptionId!==vote.a.NOT_VOTE,isSelected:selectedOptionId===voteOption.id,handleVoteClick:()=>handleVoteClick(voteOption.id)},voteOption.id)))})}WrittenVoteOptionList.displayName="WrittenVoteOptionList";try{WrittenVoteOptionList.displayName="WrittenVoteOptionList",WrittenVoteOptionList.__docgenInfo={description:"",displayName:"WrittenVoteOptionList",props:{isPreview:{defaultValue:null,description:"",name:"isPreview",required:!0,type:{name:"boolean"}},selectedOptionId:{defaultValue:null,description:"",name:"selectedOptionId",required:!0,type:{name:"number"}},voteOptionList:{defaultValue:null,description:"",name:"voteOptionList",required:!0,type:{name:"WrittenVoteOptionType[]"}},handleVoteClick:{defaultValue:null,description:"",name:"handleVoteClick",required:!0,type:{name:"(newOptionId: number) => void"}}}},"undefined"!=typeof STORYBOOK_REACT_CLASSES&&(STORYBOOK_REACT_CLASSES["src/components/optionList/WrittenVoteOptionList/index.tsx#WrittenVoteOptionList"]={docgenInfo:WrittenVoteOptionList.__docgenInfo,name:"WrittenVoteOptionList",path:"src/components/optionList/WrittenVoteOptionList/index.tsx#WrittenVoteOptionList"})}catch(__react_docgen_typescript_loader_error){}},"./src/constants/path.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{G:()=>BASE_PATH,m:()=>PATH});const BASE_PATH={HOME:"/",LANDING:"/landing",LOGIN:"/login",POST:"/posts",USER:"/users",ADMIN:"/admin",SEARCH:"/search"},PATH={...BASE_PATH,POST_WRITE:`${BASE_PATH.POST}/write`,POST_VOTE_RESULT:`${BASE_PATH.POST}/result`,POST_CATEGORY:`${BASE_PATH.POST}/category`,USER_POST:`${BASE_PATH.USER}/posts`,USER_VOTE:`${BASE_PATH.USER}/votes`,USER_INFO:`${BASE_PATH.USER}/myPage`}},"./src/constants/vote.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{a:()=>POST});const POST={NOT_VOTE:0}},"./src/styles/theme.ts":(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{__webpack_require__.d(__webpack_exports__,{r:()=>theme});const theme={breakpoint:{sm:"576px",md:"768px",lg:"1440px"},zIndex:{header:100,modal:200}}}}]);
+//# sourceMappingURL=components-common-Post-Post-stories.4ec17fce.iframe.bundle.js.map

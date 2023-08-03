ae03f9a3f30205d3a2c8369f720f48e94b77eff2
aiaiaiai1 <cvc100326@naver.com>
2023-08-03 14:10:50 +0900

    style: (#80) import문 정리

--- backend/src/main/java/com/votogether/domain/post/repository/PostRepository.java
+++ backend/src/main/java/com/votogether/domain/post/repository/PostRepository.java
@@ -3,7 +3,6 @@ package com.votogether.domain.post.repository;
 import com.votogether.domain.member.entity.Member;
 import com.votogether.domain.post.entity.Post;
 import java.time.LocalDateTime;
-import java.util.List;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;

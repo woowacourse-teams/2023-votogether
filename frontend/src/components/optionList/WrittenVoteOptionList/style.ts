f9dac69936f1b4b854aa779bf065aac5824d6146
chsua <csj1919@naver.com>
2023-08-03 13:37:59 +0900

    feat: (#203)  선택지 태그 목적에 맞게 수정(ul-li > div-button)

--- frontend/src/components/optionList/WrittenVoteOptionList/style.ts
+++ frontend/src/components/optionList/WrittenVoteOptionList/style.ts
@@ -2,7 +2,7 @@ import { styled } from 'styled-components';
 
 import { theme } from '@styles/theme';
 
-export const VoteOptionListContainer = styled.ul`
+export const VoteOptionListContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;

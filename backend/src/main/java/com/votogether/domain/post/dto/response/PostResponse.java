901276380cb8cf0556dba3bf8f0dcadf2f1ec980
jeomxon <jeomxon@gmail.com>
2023-08-05 16:56:17 +0900

    refactor: (#164) 닉네임 getter 추가

--- backend/src/main/java/com/votogether/domain/post/dto/response/PostResponse.java
+++ backend/src/main/java/com/votogether/domain/post/dto/response/PostResponse.java
@@ -24,7 +24,7 @@ public record PostResponse(
 
         return new PostResponse(
                 post.getId(),
-                WriterResponse.of(writer.getId(), writer.getNickname().getValue()),
+                WriterResponse.of(writer.getId(), writer.getNickname()),
                 postBody.getTitle(),
                 postBody.getContent(),
                 getCategories(post),

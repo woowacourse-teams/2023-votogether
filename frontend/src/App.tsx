f1d848de93e6012e6c88eb3547fda4159d9d800c
gilpop8663 <wolfye@naver.com>
2023-08-07 22:05:55 +0900

    feat: (#282) 게시글 옵션을 ContextApi를 이용해 전역으로 관리 상세 게시글을 보다가 뒤로가기를 하여도 원래 보던 옵션 게시글 목록이 나오도록 하기 위해서

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 
 import { AuthProvider } from '@hooks/context/auth';
+import PostOptionProvider from '@hooks/context/postOption';
 
 import router from '@routes/router';
 
@@ -15,10 +16,12 @@ const queryClient = new QueryClient();
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <AuthProvider>
-      <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <RouterProvider router={router} />
-      </ThemeProvider>
+      <PostOptionProvider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <RouterProvider router={router} />
+        </ThemeProvider>
+      </PostOptionProvider>
     </AuthProvider>
   </QueryClientProvider>
 );
---
+++ frontend/src/hooks/context/postOption.tsx
@@ -0,0 +1,32 @@
+import { PropsWithChildren, createContext, useState } from 'react';
+
+import type { PostSorting, PostStatus } from '@components/post/PostListPage/types';
+
+import { SORTING, STATUS } from '@constants/post';
+
+export const PostOptionContext = createContext<PostOptionContextProps>({
+  postOption: { sorting: SORTING.LATEST, status: STATUS.PROGRESS },
+  setPostOption: () => {},
+});
+
+interface PostOption {
+  status: PostStatus;
+  sorting: PostSorting;
+}
+interface PostOptionContextProps {
+  postOption: PostOption;
+  setPostOption: (postOption: PostOption) => void;
+}
+
+export default function PostOptionProvider({ children }: PropsWithChildren) {
+  const [postOption, setPostOption] = useState<PostOption>({
+    sorting: SORTING.LATEST,
+    status: STATUS.PROGRESS,
+  });
+
+  return (
+    <PostOptionContext.Provider value={{ postOption, setPostOption }}>
+      {children}
+    </PostOptionContext.Provider>
+  );
+}

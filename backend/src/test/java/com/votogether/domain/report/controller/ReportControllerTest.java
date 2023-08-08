bb24e4226d795bcf0241f7a22e6d3d79e97bc65e
jeomxon <jeomxon@gmail.com>
2023-08-08 13:22:09 +0900

    refactor: (#164) nested로 신고 기능 묶기

--- backend/src/test/java/com/votogether/domain/report/controller/ReportControllerTest.java
+++ backend/src/test/java/com/votogether/domain/report/controller/ReportControllerTest.java
@@ -18,6 +18,7 @@ import io.restassured.http.ContentType;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -46,137 +47,109 @@ class ReportControllerTest {
         RestAssuredMockMvc.webAppContextSetup(webApplicationContext);
     }
 
-    @Test
-    @DisplayName("게시글 신고를 한다.")
-    void reportPost() throws Exception {
-        // given
-        Member member = Member.builder()
-                .nickname("저문")
-                .gender(Gender.MALE)
-                .ageRange("20~29")
-                .socialId("abc123")
-                .socialType(SocialType.KAKAO)
-                .birthday("0101")
-                .point(1234)
-                .build();
+    @Nested
+    @DisplayName("신고 기능은 ")
+    class Report {
 
-        TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
-        given(tokenProcessor.resolveToken(anyString())).willReturn("token");
-        given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
-        given(memberService.findById(anyLong())).willReturn(member);
+        @Test
+        @DisplayName("게시글에 대해 정상적으로 작동한다.")
+        void reportPost() throws Exception {
+            // given
+            Member member = Member.builder()
+                    .nickname("저문")
+                    .gender(Gender.MALE)
+                    .ageRange("20~29")
+                    .socialId("abc123")
+                    .socialType(SocialType.KAKAO)
+                    .birthday("0101")
+                    .point(1234)
+                    .build();
 
-        ReportRequest request = new ReportRequest(ReportType.POST.name(), 1L, "불건전한 게시글");
-        willDoNothing().given(reportService).report(member, request);
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(member);
 
-        // when, then
-        RestAssuredMockMvc
-                .given().log().all()
-                .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when().post("/report")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-    }
+            ReportRequest request = new ReportRequest(ReportType.POST, 1L, "불건전한 게시글");
+            willDoNothing().given(reportService).report(member, request);
 
-    @Test
-    @DisplayName("댓글 신고를 한다.")
-    void reportComment() throws Exception {
-        // given
-        Member member = Member.builder()
-                .nickname("저문")
-                .gender(Gender.MALE)
-                .ageRange("20~29")
-                .socialId("abc123")
-                .socialType(SocialType.KAKAO)
-                .birthday("0101")
-                .point(1234)
-                .build();
+            // when, then
+            RestAssuredMockMvc
+                    .given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when().post("/report")
+                    .then().log().all()
+                    .statusCode(HttpStatus.OK.value());
+        }
 
-        TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
-        given(tokenProcessor.resolveToken(anyString())).willReturn("token");
-        given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
-        given(memberService.findById(anyLong())).willReturn(member);
+        @Test
+        @DisplayName("댓글에 대해 정상적으로 작동한다.")
+        void reportComment() throws Exception {
+            // given
+            Member member = Member.builder()
+                    .nickname("저문")
+                    .gender(Gender.MALE)
+                    .ageRange("20~29")
+                    .socialId("abc123")
+                    .socialType(SocialType.KAKAO)
+                    .birthday("0101")
+                    .point(1234)
+                    .build();
 
-        ReportRequest request = new ReportRequest(ReportType.COMMENT.name(), 1L, "불건전한 댓글");
-        willDoNothing().given(reportService).report(member, request);
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(member);
 
-        // when, then
-        RestAssuredMockMvc
-                .given().log().all()
-                .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when().post("/report")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-    }
+            ReportRequest request = new ReportRequest(ReportType.COMMENT, 1L, "불건전한 댓글");
+            willDoNothing().given(reportService).report(member, request);
 
-    @Test
-    @DisplayName("닉네임 신고를 한다.")
-    void reportNickname() throws Exception {
-        // given
-        Member member = Member.builder()
-                .nickname("저문")
-                .gender(Gender.MALE)
-                .ageRange("20~29")
-                .socialId("abc123")
-                .socialType(SocialType.KAKAO)
-                .birthday("0101")
-                .point(1234)
-                .build();
+            // when, then
+            RestAssuredMockMvc
+                    .given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when().post("/report")
+                    .then().log().all()
+                    .statusCode(HttpStatus.OK.value());
+        }
 
-        TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
-        given(tokenProcessor.resolveToken(anyString())).willReturn("token");
-        given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
-        given(memberService.findById(anyLong())).willReturn(member);
+        @Test
+        @DisplayName("닉네임에 대해 정상적으로 작동한다.")
+        void reportNickname() throws Exception {
+            // given
+            Member member = Member.builder()
+                    .nickname("저문")
+                    .gender(Gender.MALE)
+                    .ageRange("20~29")
+                    .socialId("abc123")
+                    .socialType(SocialType.KAKAO)
+                    .birthday("0101")
+                    .point(1234)
+                    .build();
 
-        ReportRequest request = new ReportRequest(ReportType.NICKNAME.name(), 1L, "불건전한 닉네임");
-        willDoNothing().given(reportService).report(member, request);
+            TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
+            given(tokenProcessor.resolveToken(anyString())).willReturn("token");
+            given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
+            given(memberService.findById(anyLong())).willReturn(member);
 
-        // when, then
-        RestAssuredMockMvc
-                .given().log().all()
-                .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when().post("/report")
-                .then().log().all()
-                .statusCode(HttpStatus.OK.value());
-    }
+            ReportRequest request = new ReportRequest(ReportType.NICKNAME, 1L, "불건전한 닉네임");
+            willDoNothing().given(reportService).report(member, request);
 
-    @ParameterizedTest
-    @NullAndEmptySource
-    @DisplayName("신고 대상 타입이 빈 값일 경우 400을 반환한다.")
-    void reportTypeBadRequest(String type) throws Exception {
-        // given
-        Member member = Member.builder()
-                .nickname("저문")
-                .gender(Gender.MALE)
-                .ageRange("20~29")
-                .socialId("abc123")
-                .socialType(SocialType.KAKAO)
-                .birthday("0101")
-                .point(1234)
-                .build();
+            // when, then
+            RestAssuredMockMvc
+                    .given().log().all()
+                    .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
+                    .contentType(ContentType.JSON)
+                    .body(request)
+                    .when().post("/report")
+                    .then().log().all()
+                    .statusCode(HttpStatus.OK.value());
+        }
 
-        TokenPayload tokenPayload = new TokenPayload(1L, 1L, 1L);
-        given(tokenProcessor.resolveToken(anyString())).willReturn("token");
-        given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
-        given(memberService.findById(anyLong())).willReturn(member);
-
-        ReportRequest request = new ReportRequest(type, 1L, "불건전한 게시글");
-        willDoNothing().given(reportService).report(member, request);
-
-        // when, then
-        RestAssuredMockMvc
-                .given().log().all()
-                .headers(HttpHeaders.AUTHORIZATION, "Bearer token")
-                .contentType(ContentType.JSON)
-                .body(request)
-                .when().post("/report")
-                .then().log().all()
-                .statusCode(HttpStatus.BAD_REQUEST.value());
     }
 
     @ParameterizedTest
@@ -199,7 +172,7 @@ class ReportControllerTest {
         given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
         given(memberService.findById(anyLong())).willReturn(member);
 
-        ReportRequest request = new ReportRequest(ReportType.COMMENT.name(), id, "불건전한 게시글");
+        ReportRequest request = new ReportRequest(ReportType.COMMENT, id, "불건전한 게시글");
         willDoNothing().given(reportService).report(member, request);
 
         // when, then
@@ -233,7 +206,7 @@ class ReportControllerTest {
         given(tokenProcessor.parseToken(anyString())).willReturn(tokenPayload);
         given(memberService.findById(anyLong())).willReturn(member);
 
-        ReportRequest request = new ReportRequest(ReportType.POST.name(), 1L, reason);
+        ReportRequest request = new ReportRequest(ReportType.POST, 1L, reason);
         willDoNothing().given(reportService).report(member, request);
 
         // when, then

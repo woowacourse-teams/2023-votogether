5098defb1b633250393cea9fdf8a3b5ff737192c
jeomxon <jeomxon@gmail.com>
2023-08-08 16:14:03 +0900

    refactor: (#164) Report 엔티티 클래스에 컬럼 복합 인덱스 설정

--- backend/src/main/java/com/votogether/domain/report/entity/Report.java
+++ backend/src/main/java/com/votogether/domain/report/entity/Report.java
@@ -10,6 +10,7 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -19,9 +20,10 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(uniqueConstraints = {
-        @UniqueConstraint(columnNames = {"member_id", "reportType", "targetId"})
-})
+@Table(
+        uniqueConstraints = {@UniqueConstraint(columnNames = {"member_id", "reportType", "targetId"})},
+        indexes = {@Index(columnList = "reportType, targetId")}
+)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity

3d2da36675486c1b8f6cfca46daa1f9f6c9f7cc9
chsua <csj1919@naver.com>
2023-08-08 22:41:59 +0900

    test: (#167) 게시물 삭제/신고 msw 생성 및 테스트

--- frontend/src/api/report.ts
+++ frontend/src/api/report.ts
@@ -2,7 +2,7 @@ import { ReportRequest } from '@type/report';
 
 import { postFetch } from '@utils/fetch';
 
-const BASE_URL = process.env.VOTOGETHER_BASE_URL;
+const BASE_URL = process.env.VOTOGETHER_MOCKING_URL;
 
 export const reportContent = async (reportData: ReportRequest) => {
   return await postFetch(`${BASE_URL}/report`, reportData);

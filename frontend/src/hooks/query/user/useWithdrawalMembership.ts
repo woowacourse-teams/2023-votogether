4b6b22782bbdcc0e80c2f53fa1f26172585fca03
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2023-08-09 14:24:00 +0900

    회원 닉네임 수정, 회원 탈퇴 react-query를 이용한 커스텀훅 구현 (#192)
    * feat: (#187) 유저가 닉네임을 변경하는 커스텀 훅 구현
    
    * feat: (#187) 유저 회원 탈퇴 기능 커스텀 훅 구현
    
    * feat: (#187) 닉네임 변경 부분 낙관적 업데이트 적용
    
    * refactor: (#187) 닉네임 변경에 실패했을 경우 console.error 코드 추가
    
    * refactor: (#187) 회원 탈퇴를 cancel에서 withdrawal로 변경
    
    * refactor: (#187) dev 브런치 머지 후 파일 경로 재설정

---
+++ frontend/src/hooks/query/user/useWithdrawalMembership.ts
@@ -0,0 +1,20 @@
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { withdrawalMembership } from '@api/userInfo';
+
+import { QUERY_KEY } from '@constants/queryKey';
+
+export const useWithdrawalMembership = () => {
+  const queryClient = useQueryClient();
+  const { mutate, isLoading } = useMutation({
+    mutationFn: () => withdrawalMembership(),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: [QUERY_KEY.USER_INFO] });
+    },
+    onError: () => {
+      window.console.error('회원 탈퇴에 실패했습니다.');
+    },
+  });
+
+  return { mutate, isLoading };
+};

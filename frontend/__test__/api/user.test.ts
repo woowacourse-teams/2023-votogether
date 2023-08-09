ab7db7a9c25589a5f123a3f27d78f896fee47d52
Gilpop8663 <wolfye@naver.com>
2023-08-09 16:49:42 +0900

    test: (#299) 비회원일때 유저 정보를 불러오는 테스트 케이스 추가

--- frontend/__test__/api/user.test.ts
+++ frontend/__test__/api/user.test.ts
@@ -8,6 +8,12 @@ import {
 import { MOCK_USER_INFO } from '@mocks/mockData/user';
 
 describe('서버와 통신하여 유저의 정보를 불러올 수 있어야 한다.', () => {
+  test('비회원일때는 null을 반환한다.', async () => {
+    const data = await getUserInfo(false);
+
+    expect(data).toEqual(null);
+  });
+
   test('유저의 정보를 불러온다', async () => {
     const data = await getUserInfo(true);
 

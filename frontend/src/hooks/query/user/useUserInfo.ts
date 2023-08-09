37441e26030a114e70930c7d4bbc9d6001480521
Gilpop8663 <wolfye@naver.com>
2023-08-09 17:19:01 +0900

    feat: (#308) 유저 정보, 카테고리 목록에서 캐싱, 스테일 타임을 1시간으로 수정

--- frontend/src/hooks/query/user/useUserInfo.ts
+++ frontend/src/hooks/query/user/useUserInfo.ts
@@ -9,7 +9,11 @@ import { QUERY_KEY } from '@constants/queryKey';
 export const useUserInfo = (isLogged: boolean) => {
   const { data, error, isLoading, isError } = useQuery<User>(
     [QUERY_KEY.USER_INFO, isLogged],
-    getUserInfo
+    getUserInfo,
+    {
+      cacheTime: 60 * 60 * 1000,
+      staleTime: 60 * 60 * 1000,
+    }
   );
 
   return { data, error, isLoading, isError };

07a4f6b1eaa4e1c3d3db3b5bad3fdab922663ac4
gilpop8663 <wolfye@naver.com>
2023-08-10 17:09:19 +0900

    test: (#292) 투표 테스트에서 given을 구체화

--- frontend/__test__/api/vote.test.ts
+++ frontend/__test__/api/vote.test.ts
@@ -4,7 +4,10 @@ import { MOCK_POST_INFO } from '@mocks/mockData/post';
 
 describe('서버와 통신하여 유저의 정보를 불러올 수 있어야 한다.', () => {
   test('유저의 정보를 불러온다', async () => {
-    await votePost(1, 2);
+    const postId = 1;
+    const optionId = 2;
+
+    await votePost(postId, optionId);
 
     expect(MOCK_POST_INFO.voteInfo.selectedOptionId).toBe(999);
   });

ed1e23ee48a98babc83511d8aca89088e781c9c1
aiaiaiai1 <cvc100326@naver.com>
2023-08-11 14:38:31 +0900

    refactor: (#289) swagger 예외 응답 어노테이션 추가

--- backend/src/main/java/com/votogether/domain/post/controller/PostController.java
+++ backend/src/main/java/com/votogether/domain/post/controller/PostController.java
@@ -187,6 +187,7 @@ public class PostController {
     @Operation(summary = "게시글 검색(회원)", description = "회원으로 키워드를 통해 게시글을 검색한다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "게시글 검색 성공"),
+            @ApiResponse(responseCode = "400", description = "잘못된 쿼리 파라미터값 입력"),
     })
     @GetMapping("/search")
     public ResponseEntity<List<PostResponse>> searchPostsWithKeyword(

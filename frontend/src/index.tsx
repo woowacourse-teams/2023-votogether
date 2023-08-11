1489490be514d96d6e26b9f529ac43eda614b227
Gilpop8663 <wolfye@naver.com>
2023-08-11 14:16:28 +0900

    feat: (#310) PWA(프로그레시브 웹 앱) 요소를 충족시키는 조건 설정

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,16 +2,12 @@ import React from 'react';
 
 import ReactDOM from 'react-dom/client';
 
-import { promptAddToHomeScreen } from '@utils/promptAddToHomeScreen';
-
 import App from './App';
 import { worker } from './mocks/worker';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start();
 }
-
-promptAddToHomeScreen();
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(

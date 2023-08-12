8672de0b2560191c42981678ca35605bc8ad3792
chsua <csj1919@naver.com>
2023-08-12 16:50:13 +0900

    fix: 우스가 수정한 코드 도입

--- frontend/src/pages/ErrorBoundary.tsx
+++ frontend/src/pages/ErrorBoundary.tsx
@@ -2,34 +2,31 @@ import { Component, ErrorInfo, ReactNode } from 'react';
 
 interface ErrorBoundaryProps {
   children: ReactNode;
-  fallback: ReactNode;
 }
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  errorMessage: string;
 }
 
 class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
-  #errorMessage = '';
-
   constructor(props: ErrorBoundaryProps) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, errorMessage: '' };
   }
 
   static getDerivedStateFromError(error: Error) {
-    return { hasError: true };
+    return { hasError: true, errorMessage: error.message };
   }
 
   componentDidCatch(error: Error, errorInfo: ErrorInfo) {
     // You can also log the error to an error reporting service
     window.console.log(error, errorInfo);
-    this.#errorMessage = error.message;
   }
 
   render() {
     if (this.state.hasError) {
-      return <div>{this.#errorMessage}</div>;
+      return <div>{this.state.errorMessage}</div>;
     }
 
     return this.props.children;

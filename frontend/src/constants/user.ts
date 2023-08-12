8df62b66043596baa15e69a77df964700b3d6eee
inyeong-kang <tomatorkd@naver.com>
2023-08-12 13:45:56 +0900

    chore: (#344) 불필요한 코드 ㄷ삭제

--- frontend/src/constants/user.ts
+++ frontend/src/constants/user.ts
@@ -1,11 +1,9 @@
-import { UserInfoLength } from '@type/user';
-
-export const NICKNAME: UserInfoLength = {
+export const NICKNAME = {
   MAX_LENGTH: 15,
   MIN_LENGTH: 2,
 } as const;
 
-export const BIRTH_YEAR: UserInfoLength = {
+export const BIRTH_YEAR = {
   MAX_LENGTH: new Date().getFullYear(),
   MIN_LENGTH: 1900,
 } as const;

863ea586b3c565cb1942d71db0814368bcb82349
Gilpop8663 <wolfye@naver.com>
2023-08-13 14:57:01 +0900

    chore: (#327) dev 브런치와 코드 병합

--- frontend/src/pages/post/PostDetail/PostDetail/index.tsx
+++ frontend/src/pages/post/PostDetail/PostDetail/index.tsx
@@ -1,4 +1,4 @@
-import { Suspense, useContext } from 'react';
+import { Suspense, useContext, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { PostInfo } from '@type/post';
@@ -8,6 +8,7 @@ import { AuthContext } from '@hooks/context/auth';
 import { useDeletePost } from '@hooks/query/post/useDeletePost';
 import { useEarlyClosePost } from '@hooks/query/post/useEarlyClosePost';
 import { usePostDetail } from '@hooks/query/post/usePostDetail';
+import { useToast } from '@hooks/useToast';
 
 import { reportContent } from '@api/report';
 
@@ -17,6 +18,7 @@ import CommentList from '@components/comment/CommentList';
 import NarrowTemplateHeader from '@components/common/NarrowTemplateHeader';
 import Post from '@components/common/Post';
 import Skeleton from '@components/common/Skeleton';
+import Toast from '@components/common/Toast';
 
 import { checkClosedPost } from '@utils/time';
 
@@ -30,12 +32,18 @@ export default function PostDetail() {
 
   const params = useParams() as { postId: string };
   const postId = Number(params.postId);
+  const { isToastOpen, openToast, toastMessage } = useToast();
 
   const { loggedInfo } = useContext(AuthContext);
   const memberId = loggedInfo.id;
 
   const { data: postData } = usePostDetail(!loggedInfo.isLoggedIn, postId);
-  const { mutate: deletePost } = useDeletePost(postId, loggedInfo.isLoggedIn);
+  const {
+    mutate: deletePost,
+    isSuccess: isDeleteSuccess,
+    isError: isDeleteError,
+    error: deleteError,
+  } = useDeletePost(postId, loggedInfo.isLoggedIn);
   const { mutate: earlyClosePost } = useEarlyClosePost(postId);
 
   const postDataFallback = postData ?? ({} as PostInfo);
@@ -45,8 +53,9 @@ export default function PostDetail() {
 
   const movePage = {
     moveWritePostPage: () => {
-      if (postDataFallback.voteInfo.allPeopleCount)
-        alert('투표한 사용자가 있어 글 수정이 불가합니다.');
+      if (postDataFallback.voteInfo.allPeopleCount) {
+        openToast('투표한 사용자가 있어 글 수정이 불가합니다.');
+      }
 
       navigate(`/posts/write/${postId}`);
     },
@@ -61,18 +70,23 @@ export default function PostDetail() {
   const controlPost = {
     setEarlyClosePost: earlyClosePost,
     deletePost: () => {
-      if (postDataFallback.voteInfo.allPeopleCount >= 20)
-        return alert('20인 이상 투표한 게시물은 삭제할 수 없습니다.');
+      if (postDataFallback.voteInfo.allPeopleCount >= 20) {
+        openToast('20인 이상 투표한 게시물은 삭제할 수 없습니다.');
+        return;
+      }
 
       deletePost();
-      //추후 삭제가 되었을 때 nav로 홈으로 이동하도록 하기
     },
     reportPost: async (reason: string) => {
       const reportData: ReportRequest = { type: 'POST', id: postId, reason };
 
       await reportContent(reportData)
-        .then(res => alert('게시물을 신고했습니다.'))
-        .catch(error => alert('게시물 신고가 실패했습니다.'));
+        .then(res => {
+          openToast('게시물을 신고했습니다.');
+        })
+        .catch(e => {
+          openToast(e instanceof Error ? e.message : '게시물 신고가 실패했습니다');
+        });
     },
     reportNickname: async (reason: string) => {
       const reportData: ReportRequest = {
@@ -82,10 +96,26 @@ export default function PostDetail() {
       };
 
       await reportContent(reportData)
-        .then(res => alert('작성자 닉네임을 신고했습니다.'))
-        .catch(error => alert('작성자 닉네임 신고가 실패했습니다.'));
+        .then(res => {
+          openToast('작성자 닉네임을 신고했습니다.');
+        })
+        .catch(e => {
+          openToast(e instanceof Error ? e.message : '작성자 닉네임 신고가 실패했습니다.');
+        });
     },
   };
+
+  useEffect(() => {
+    if (isDeleteError && deleteError instanceof Error) {
+      openToast(deleteError.message);
+    }
+  }, [isDeleteError, deleteError]);
+
+  useEffect(() => {
+    if (isDeleteSuccess) {
+      navigate('/');
+    }
+  }, [isDeleteSuccess, navigate]);
 
   return (
     <>
@@ -113,6 +143,11 @@ export default function PostDetail() {
           </Suspense>
         </ErrorBoundary>
       </S.BottomContainer>
+      {isToastOpen && (
+        <Toast size="md" position="bottom">
+          {toastMessage}
+        </Toast>
+      )}
     </>
   );
 }

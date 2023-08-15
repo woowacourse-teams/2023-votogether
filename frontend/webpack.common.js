70f44b9b725f3972a7723545dc81c921e4caddb3
gilpop8663 <wolfye@naver.com>
2023-08-15 13:12:41 +0900

    chore: (#405) 웹팩 설정에서 svg 파일은 빌드 시 코드에 포함되도록 수정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -46,7 +46,11 @@ module.exports = {
         use: ['style-loader', 'css-loader'],
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        test: /\.svg/,
+        type: 'asset/inline',
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
     ],

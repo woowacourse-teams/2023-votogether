20ded3b23bf8d4d5441e7ff758c5cb617c6e2426
김영길/KIM YOUNG GIL <80146176+Gilpop8663@users.noreply.github.com>
2023-08-17 19:48:33 +0900

    인터넷 익스플로어로 접속 시 브라우저를 업데이트 해달라는 기능 구현 (#452)
    * feat: (#447) 인터넷 익스플로어로 접속 시 브라우저를 업데이트 해달라는 기능 구현
    
    * chore: (#447) 사진 정책 변경
    
    * chore: (#447) 사진 이미지를 프론트에서 1.5MB로 제한하도록 수정
    
    * fix: (#447) 존재하지 않는 role 삭제

--- frontend/src/components/PostForm/constants.ts
+++ frontend/src/components/PostForm/constants.ts
@@ -2,4 +2,4 @@ export type DeadlineOption = '10분' | '30분' | '1시간' | '6시간' | '1일';
 
 export const DEADLINE_OPTION: DeadlineOption[] = ['10분', '30분', '1시간', '6시간', '1일'];
 
-export const MAX_FILE_SIZE = 5000000;
+export const MAX_FILE_SIZE = 1500000;

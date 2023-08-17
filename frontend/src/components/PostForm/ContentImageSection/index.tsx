0e1ee3c13aa52ee5b7d18d9f9901af7436eb1bd4
chsua <csj1919@naver.com>
2023-08-17 16:42:20 +0900

    feat: (#453) 글작성 폼에서 인터렉션하는 태그는 tab되도록 수정

--- frontend/src/components/PostForm/ContentImageSection/index.tsx
+++ frontend/src/components/PostForm/ContentImageSection/index.tsx
@@ -18,6 +18,10 @@ interface ContentImageSectionProps {
 export default function ContentImageSection({ contentImageHook, size }: ContentImageSectionProps) {
   const { contentImage, contentInputRef, removeImage, handleUploadImage } = contentImageHook;
 
+  const handleButtonClick = () => {
+    contentInputRef.current && contentInputRef.current.click();
+  };
+
   return (
     <>
       {contentImage && (
@@ -30,20 +34,21 @@ export default function ContentImageSection({ contentImageHook, size }: ContentI
       )}
       {
         <S.FileInputContainer>
-          <S.Label
-            htmlFor="content-image-upload"
-            aria-label="본문 이미지 업로드 버튼"
-            title="이미지 업로드"
+          <S.Button
+            type="button"
             $isVisible={!!contentImage}
+            aria-label="본문 이미지 업로드"
+            onClick={handleButtonClick}
           >
-            본문에 사진 넣기
-          </S.Label>
+            <S.Label htmlFor="content-image-upload">본문에 사진 넣기</S.Label>
+          </S.Button>
           <S.FileInput
             id="content-image-upload"
             ref={contentInputRef}
             type="file"
             accept="image/*"
             onChange={handleUploadImage}
+            tabIndex={-1}
           />
         </S.FileInputContainer>
       }
--- frontend/src/components/optionList/WritingVoteOptionList/WritingVoteOption/OptionUploadImageButton/index.tsx
+++ frontend/src/components/optionList/WritingVoteOptionList/WritingVoteOption/OptionUploadImageButton/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import photoIcon from '@assets/photo_white.svg';
 
@@ -14,14 +14,21 @@ export default function OptionUploadImageButton({
   isImageVisible,
   ...rest
 }: OptionUploadImageButtonProps) {
+  const inputRef = useRef<HTMLInputElement>(null);
   const id = optionId.toString();
+
+  const handleButtonClick = () => {
+    inputRef.current && inputRef.current.click();
+  };
 
   return (
     <S.Container $isVisible={isImageVisible}>
-      <S.Label htmlFor={id} aria-label="선택지 이미지 업로드 버튼" title="이미지 업로드">
-        <S.Image src={photoIcon} alt="" />
-      </S.Label>
-      <S.FileInput id={id} type="file" accept="image/*" {...rest} />
+      <button type="button" aria-label="선택지 이미지 업로드" onClick={handleButtonClick}>
+        <S.Label htmlFor={id}>
+          <S.Image src={photoIcon} alt="" />
+        </S.Label>
+      </button>
+      <S.FileInput id={id} type="file" accept="image/*" tabIndex={-1} ref={inputRef} {...rest} />
     </S.Container>
   );
 }

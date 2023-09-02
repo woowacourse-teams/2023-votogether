b5638d8903979bcd619b3af8fe04fdd2f85fbe01
chsua <csj1919@naver.com>
2023-09-03 01:06:16 +0900

    feat: (#504) 랭킹 관련 쿼리키 추가

--- frontend/src/constants/queryKey.ts
+++ frontend/src/constants/queryKey.ts
@@ -4,4 +4,6 @@ export const QUERY_KEY = {
   COMMENTS: 'comments',
   CATEGORIES: 'categories',
   USER_INFO: 'user_info',
+  PASSION_RANKING: 'passionRanking',
+  POPULAR_RANKING: 'popularRanking',
 };

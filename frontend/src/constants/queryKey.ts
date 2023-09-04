0b14e0283351669263ebc087066d0ba27b430c7e
chsua <113416448+chsua@users.noreply.github.com>
2023-09-04 17:44:38 +0900

    랭킹 시스템 패치 함수 및 훅 만들기 (#506)
    * feat: (#504) 랭킹 관련 fetch하는 함수 만들기
    
    * feat: (#504) 랭킹 관련 쿼리키 추가
    
    * feat: (#504) 랭킹 관련 리액트쿼리 훅 제작
    
    - 열정유저, 인기글, 사용자랭킹 get
    
    * feat: (#504) 랭킹 관련 타입 정의
    
    * feat: (#504) 확정된 랭킹 관련 api url 설정
    
    * feat: (#504) 확정된 랭킹 관련 type을 api, query hook에 적용

--- frontend/src/constants/queryKey.ts
+++ frontend/src/constants/queryKey.ts
@@ -4,4 +4,6 @@ export const QUERY_KEY = {
   COMMENTS: 'comments',
   CATEGORIES: 'categories',
   USER_INFO: 'user_info',
+  PASSION_RANKING: 'passion_ranking',
+  POPULAR_RANKING: 'popular_ranking',
 };

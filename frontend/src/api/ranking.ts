8790c7768f1dcdf5aacf46c61448689c32a516a3
chsua <csj1919@naver.com>
2023-09-04 17:31:38 +0900

    feat: (#504) 확정된 랭킹 관련 api url 설정

--- frontend/src/api/ranking.ts
+++ frontend/src/api/ranking.ts
@@ -3,18 +3,15 @@ import { getFetch } from '@utils/fetch';
 const BASE_URL = process.env.VOTOGETHER_MOCKING_URL;
 
 export const getUserRanking = async (isLoggedIn: boolean) => {
-  //임의의 url
   if (!isLoggedIn) return null;
 
-  return await getFetch(`${BASE_URL}/myInfo/ranking`);
+  return await getFetch(`${BASE_URL}/members/me/ranking`);
 };
 
 export const getPassionUserRanking = async () => {
-  //임의의 url
-  return await getFetch(`${BASE_URL}/ranking/passion`);
+  return await getFetch(`${BASE_URL}/members/ranking/passion/guest`);
 };
 
 export const getPopularPostRanking = async () => {
-  //임의의 url
-  return await getFetch(`${BASE_URL}/ranking/popular`);
+  return await getFetch(`${BASE_URL}/posts/ranking/popular/guest`);
 };

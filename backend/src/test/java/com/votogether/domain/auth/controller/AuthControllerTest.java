3365ebdc69ec42a414702266a00ce76a1a2c54ab
jeomxon <jeomxon@gmail.com>
2023-09-08 13:54:23 +0900

    test: (#532) 인증 토큰 재발급 및 갱신 토큰 삭제 요청에 대한 검증 추가

--- backend/src/test/java/com/votogether/domain/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/votogether/domain/auth/controller/AuthControllerTest.java
@@ -1,14 +1,21 @@
 package com.votogether.domain.auth.controller;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.willDoNothing;
 
+import com.votogether.domain.auth.dto.request.AccessTokenRequest;
+import com.votogether.domain.auth.dto.response.AccessTokenResponse;
 import com.votogether.domain.auth.service.AuthService;
 import com.votogether.domain.auth.service.dto.LoginTokenResponse;
+import com.votogether.domain.auth.service.dto.TokenResponse;
 import com.votogether.domain.member.service.MemberService;
 import com.votogether.global.jwt.TokenProcessor;
+import io.restassured.http.Cookie;
 import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -16,6 +23,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 
 @WebMvcTest(controllers = AuthController.class)
 class AuthControllerTest {
@@ -61,6 +69,54 @@ class AuthControllerTest {
                 () -> assertThat(responseBody).contains("hasEssentialInfo"),
                 () -> assertThat(responseBody).contains("false")
         );
+    }
+
+    @Test
+    @DisplayName("인증 토큰을 재발급한다.")
+    void reissueAccessToken() throws Exception {
+        // given
+        String accessToken = "abcdefg";
+        String refreshToken = "adfdsfdsa";
+        final AccessTokenRequest request = new AccessTokenRequest(accessToken);
+        final TokenResponse tokenResponse = new TokenResponse(accessToken, refreshToken);
+        final Cookie cookie = new Cookie.Builder("refreshToken", refreshToken).build();
+
+        given(authService.reissueAuthToken(any(AccessTokenRequest.class), anyString())).willReturn(tokenResponse);
+
+        // when
+        final AccessTokenResponse response = RestAssuredMockMvc
+                .given().log().all()
+                .cookie(cookie)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(request)
+                .when().post("/auth/silent-login")
+                .then().log().all()
+                .status(HttpStatus.OK)
+                .cookie("refreshToken", tokenResponse.refreshToken())
+                .extract()
+                .as(AccessTokenResponse.class);
+
+        // then
+        assertThat(response.accessToken()).isEqualTo(tokenResponse.accessToken());
+    }
+
+    @Test
+    @DisplayName("로그아웃을 한다.")
+    void logout() {
+        // given
+        String refreshToken = "adfdsfdsa";
+        final Cookie cookie = new Cookie.Builder("refreshToken", refreshToken).build();
+
+        willDoNothing().given(authService).deleteRefreshToken(refreshToken);
+
+        // when
+        RestAssuredMockMvc
+                .given().log().all()
+                .cookie(cookie)
+                .when().delete("/auth/logout")
+                .then().log().all()
+                .header("Set-Cookie", containsString("Max-Age=0"))
+                .status(HttpStatus.NO_CONTENT);
     }
 
 }

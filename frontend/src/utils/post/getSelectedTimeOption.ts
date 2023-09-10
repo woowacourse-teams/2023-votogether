07ab5f82c22432598222516598cf03626fa2b633
chsua <csj1919@naver.com>
2023-09-10 17:41:08 +0900

    fix: 선택된 time 객체 확인을 위해 비교하는 로직을 깊은 비교로 수정

--- frontend/src/utils/post/getSelectedTimeOption.ts
+++ frontend/src/utils/post/getSelectedTimeOption.ts
@@ -5,5 +5,9 @@ import { DEADLINE_OPTION, DeadlineOptionName } from '@components/PostForm/consta
 export const getSelectedTimeOption = (time: Time): DeadlineOptionName | '사용자지정' | null => {
   if (time.day === 0 && time.hour === 0 && time.minute === 0) return null;
 
-  return DEADLINE_OPTION.find(option => option.time === time)?.name ?? '사용자지정';
+  const stringTime = JSON.stringify(time);
+
+  return (
+    DEADLINE_OPTION.find(option => JSON.stringify(option.time) === stringTime)?.name ?? '사용자지정'
+  );
 };

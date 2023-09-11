67c15cbb409e1e53c23581af619bb5e1260613ef
lookh <103165859+aiaiaiai1@users.noreply.github.com>
2023-09-11 21:34:18 +0900

    내 랭킹정보 조회 기능 (#522)
    * feat: (#515) 모든 멤버의 게시글 작성 개수 불러오기 메서드 추가
    
    * feat: (#515) 모든 멤버의 투표 수 불러오기 메서드 추가
    
    * feat: (#515) 랭킹 순위 구하는 클래스 구현
    
    * feat: (#515) 내 랭킹 정보 조회 API 구현
    
    * style: (#515) 주석 수정
    
    * feat: (#515) swagger 추가
    
    * style: (#515) final 키워드 추가
    
    * refactor: (#515) 응답 변수 추출
    
    * refactor: (#515) 메서드 이름 수정 및 순서 변경
    
    * refactor: (#515) 클래스와 메서드 이름 수정 및 상수 추출
    
    * refactor: (#515) 테스트 응답 검증 추가 및 변수 이름 수정
    
    * refactor: (#515) 트랜잭션 어노테이션 추가
    
    * refactor: (#515) score 반환 long으로 변환
    
    * refactor: (#515) getter 메서드 명 수정
    
    * test: (#515) assertAll 로 수정
    
    * refactor: (#515) ranking 도메인 패키지로 변경
    
    * refactor: (#515) 클래스 이름 및 메서드 이름 수정
    
    * refactor: (#515) 클래스,메서드,변수명 수정
    
    * refactor: (#515) 패키지 수정
    
    * refactor: (#515) swagger 어노테이션 명세 수정
    
    * refactor: (#515) uri 수정
    
    * fix: (#515) uri 테스트 수정

---
+++ backend/src/main/java/com/votogether/domain/ranking/service/RankingService.java
@@ -0,0 +1,51 @@
+package com.votogether.domain.ranking.service;
+
+import com.votogether.domain.member.entity.Member;
+import com.votogether.domain.member.repository.MemberRepository;
+import com.votogether.domain.post.repository.PostRepository;
+import com.votogether.domain.ranking.dto.response.RankingResponse;
+import com.votogether.domain.ranking.entity.PassionRankings;
+import com.votogether.domain.ranking.entity.vo.PassionRecord;
+import com.votogether.domain.vote.repository.VoteRepository;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class RankingService {
+
+    private final MemberRepository memberRepository;
+    private final PostRepository postRepository;
+    private final VoteRepository voteRepository;
+
+    @Transactional(readOnly = true)
+    public RankingResponse getPassionRanking(final Member member) {
+        final PassionRankings passionRankings = getPassionRankings();
+        return new RankingResponse(
+                passionRankings.getRanking(member),
+                member.getNickname(),
+                passionRankings.getActivityRecord(member).getPostCount(),
+                passionRankings.getActivityRecord(member).getVoteCount(),
+                passionRankings.getScore(member)
+        );
+    }
+
+    private PassionRankings getPassionRankings() {
+        final List<Member> members = memberRepository.findAll();
+        final List<Integer> postCounts = postRepository.findCountsByMembers(members);
+        final List<Integer> voteCounts = voteRepository.findCountsByMembers(members);
+
+        final Map<Member, PassionRecord> passionBoard = new HashMap<>();
+
+        for (int i = 0; i < members.size(); i++) {
+            passionBoard.put(members.get(i), new PassionRecord(postCounts.get(i), voteCounts.get(i)));
+        }
+
+        return new PassionRankings(passionBoard);
+    }
+
+}

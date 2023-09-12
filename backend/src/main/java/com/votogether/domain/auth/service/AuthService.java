ca031110fcb16973668ca68cc0e0513345a437c9
jeomxon <jeomxon@gmail.com>
2023-09-12 15:21:24 +0900

    refactor: (#532) 트랜잭션 처리를 위해 RedisTemplate으로 변경

--- backend/src/main/java/com/votogether/domain/auth/service/AuthService.java
+++ backend/src/main/java/com/votogether/domain/auth/service/AuthService.java
@@ -1,22 +1,20 @@
 package com.votogether.domain.auth.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.votogether.domain.auth.RefreshToken;
 import com.votogether.domain.auth.dto.request.AccessTokenRequest;
 import com.votogether.domain.auth.dto.response.KakaoMemberResponse;
 import com.votogether.domain.auth.exception.AuthExceptionType;
-import com.votogether.domain.auth.repository.RefreshTokenRepository;
 import com.votogether.domain.auth.service.dto.LoginTokenResponse;
 import com.votogether.domain.auth.service.dto.TokenResponse;
 import com.votogether.domain.auth.service.oauth.KakaoOAuthClient;
 import com.votogether.domain.member.entity.Member;
 import com.votogether.domain.member.service.MemberService;
 import com.votogether.global.exception.BadRequestException;
-import com.votogether.global.exception.NotFoundException;
 import com.votogether.global.jwt.TokenPayload;
 import com.votogether.global.jwt.TokenProcessor;
 import java.util.Objects;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -27,7 +25,7 @@ public class AuthService {
     private final KakaoOAuthClient kakaoOAuthClient;
     private final MemberService memberService;
     private final TokenProcessor tokenProcessor;
-    private final RefreshTokenRepository refreshTokenRepository;
+    private final RedisTemplate<String, Long> redisTemplate;
 
     @Transactional
     public LoginTokenResponse register(final String code) {
@@ -38,11 +36,11 @@ public class AuthService {
         final Member registeredMember = memberService.register(member);
         final String accessToken = tokenProcessor.generateAccessToken(registeredMember.getId());
         final String refreshToken = tokenProcessor.generateRefreshToken(registeredMember.getId());
-        refreshTokenRepository.save(new RefreshToken(refreshToken, registeredMember.getId()));
+        redisTemplate.opsForValue().set(refreshToken, registeredMember.getId());
         return new LoginTokenResponse(accessToken, refreshToken, registeredMember.hasEssentialInfo());
     }
 
-    @Transactional(noRollbackFor = {BadRequestException.class})
+    @Transactional
     public TokenResponse reissueAuthToken(
             final AccessTokenRequest request,
             final String refreshTokenByRequest
@@ -50,28 +48,26 @@ public class AuthService {
         tokenProcessor.validateToken(refreshTokenByRequest);
 
         final TokenPayload accessTokenPayload = tokenProcessor.parseToken(request.accessToken());
-        final RefreshToken refreshToken = refreshTokenRepository.findById(refreshTokenByRequest)
-                .orElseThrow(() -> new NotFoundException(AuthExceptionType.NONEXISTENT_REFRESH_TOKEN));
-        refreshTokenRepository.delete(refreshToken);
-        validateTokenInfo(accessTokenPayload, refreshToken);
+        final TokenPayload refreshTokenPayload = tokenProcessor.parseToken(refreshTokenByRequest);
 
-        final String newAccessToken = tokenProcessor.generateAccessToken(refreshToken.memberId());
-        final String newRefreshToken = tokenProcessor.generateRefreshToken(refreshToken.memberId());
-        refreshTokenRepository.save(new RefreshToken(newRefreshToken, refreshToken.memberId()));
+        redisTemplate.delete(refreshTokenByRequest);
+        validateTokenInfo(accessTokenPayload, refreshTokenPayload);
+
+        final String newAccessToken = tokenProcessor.generateAccessToken(accessTokenPayload.memberId());
+        final String newRefreshToken = tokenProcessor.generateRefreshToken(accessTokenPayload.memberId());
+        redisTemplate.opsForValue().set(newRefreshToken, accessTokenPayload.memberId());
         return new TokenResponse(newAccessToken, newRefreshToken);
     }
 
-    private void validateTokenInfo(final TokenPayload accessTokenPayload, final RefreshToken refreshToken) {
-        if (!Objects.equals(accessTokenPayload.memberId(), refreshToken.memberId())) {
+    private void validateTokenInfo(final TokenPayload accessTokenPayload, final TokenPayload refreshTokenPayload) {
+        if (!Objects.equals(accessTokenPayload.memberId(), refreshTokenPayload.memberId())) {
             throw new BadRequestException(AuthExceptionType.UNMATCHED_INFORMATION_BETWEEN_TOKEN);
         }
     }
 
     @Transactional
     public void deleteRefreshToken(final String refreshTokenByRequest) {
-        final RefreshToken refreshToken = refreshTokenRepository.findById(refreshTokenByRequest)
-                .orElseThrow(() -> new NotFoundException(AuthExceptionType.NONEXISTENT_REFRESH_TOKEN));
-        refreshTokenRepository.delete(refreshToken);
+        redisTemplate.delete(refreshTokenByRequest);
     }
 
 }
--- backend/src/test/java/com/votogether/domain/auth/service/AuthServiceTest.java
+++ backend/src/test/java/com/votogether/domain/auth/service/AuthServiceTest.java
@@ -3,16 +3,14 @@ package com.votogether.domain.auth.service;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import com.votogether.domain.auth.RefreshToken;
 import com.votogether.domain.auth.dto.request.AccessTokenRequest;
-import com.votogether.domain.auth.repository.RefreshTokenRepository;
 import com.votogether.domain.auth.service.dto.TokenResponse;
 import com.votogether.domain.auth.service.oauth.KakaoOAuthClient;
 import com.votogether.domain.member.service.MemberService;
 import com.votogether.global.exception.BadRequestException;
-import com.votogether.global.exception.NotFoundException;
 import com.votogether.global.jwt.TokenProcessor;
 import com.votogether.test.config.RedisTestConfig;
+import java.util.Objects;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -20,6 +18,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Import(RedisTestConfig.class)
 @SpringBootTest
@@ -38,7 +37,7 @@ class AuthServiceTest {
     TokenProcessor tokenProcessor;
 
     @Autowired
-    RefreshTokenRepository refreshTokenRepository;
+    RedisTemplate<String, Long> redisTemplate;
 
     @Nested
     @DisplayName("인증 토큰을 재발급 받을 때")
@@ -46,7 +45,7 @@ class AuthServiceTest {
 
         @AfterEach
         void tearDown() {
-            refreshTokenRepository.deleteAll();
+            Objects.requireNonNull(redisTemplate.getConnectionFactory()).getConnection().serverCommands().flushAll();
         }
 
         @Test
@@ -58,30 +57,14 @@ class AuthServiceTest {
             String refreshToken = tokenProcessor.generateRefreshToken(memberId);
             AccessTokenRequest request = new AccessTokenRequest(accessToken);
 
-            RefreshToken refreshTokenObject = new RefreshToken(refreshToken, memberId);
-            refreshTokenRepository.save(refreshTokenObject);
+            redisTemplate.opsForValue().set(refreshToken, memberId);
 
             // when
             TokenResponse tokenResponse = authService.reissueAuthToken(request, refreshToken);
 
             // then
-            RefreshToken savedRefreshToken = refreshTokenRepository.findById(tokenResponse.refreshToken()).get();
-            assertThat(tokenResponse.refreshToken()).isEqualTo(savedRefreshToken.refreshToken());
-        }
-
-        @Test
-        @DisplayName("갱신 토큰이 저장되어 있지 않다면 예외가 발생한다.")
-        void throwsExceptionWhenNonSavedRefreshToken() {
-            // given
-            Long memberId = 1L;
-            String accessToken = tokenProcessor.generateAccessToken(memberId);
-            String refreshToken = tokenProcessor.generateRefreshToken(memberId);
-            AccessTokenRequest request = new AccessTokenRequest(accessToken);
-
-            // when, then
-            assertThatThrownBy(() -> authService.reissueAuthToken(request, refreshToken))
-                    .isInstanceOf(NotFoundException.class)
-                    .hasMessage("갱신 토큰이 존재하지 않습니다.");
+            redisTemplate.opsForValue().get(tokenResponse.refreshToken());
+            assertThat(tokenResponse.refreshToken()).isEqualTo(refreshToken);
         }
 
         @Test
@@ -94,8 +77,7 @@ class AuthServiceTest {
             String refreshToken = tokenProcessor.generateRefreshToken(refreshTokenMemberId);
             AccessTokenRequest request = new AccessTokenRequest(accessToken);
 
-            RefreshToken refreshTokenObject = new RefreshToken(refreshToken, refreshTokenMemberId);
-            refreshTokenRepository.save(refreshTokenObject);
+            redisTemplate.opsForValue().set(refreshToken, refreshTokenMemberId);
 
             // when, then
             assertThatThrownBy(() -> authService.reissueAuthToken(request, refreshToken))
@@ -115,29 +97,13 @@ class AuthServiceTest {
             // given
             Long memberId = 3L;
             String refreshToken = "ihg.fed.cba";
-            refreshTokenRepository.save(new RefreshToken(refreshToken, memberId));
+            redisTemplate.opsForValue().set(refreshToken, memberId);
 
             // when
             authService.deleteRefreshToken(refreshToken);
 
             // then
-            assertThat(refreshTokenRepository.count()).isZero();
-        }
-
-        @Test
-        @DisplayName("갱신 토큰이 존재하지 않으면 예외가 발생한다.")
-        void failRefresh() {
-            // given
-            Long memberId = 5L;
-            String refreshToken = "iII.fed.cba";
-            refreshTokenRepository.save(new RefreshToken(refreshToken, memberId));
-
-            String invalidRefreshToken = "iIi.fed.cba";
-
-            // when
-            assertThatThrownBy(() -> authService.deleteRefreshToken(invalidRefreshToken))
-                    .isInstanceOf(NotFoundException.class)
-                    .hasMessage("갱신 토큰이 존재하지 않습니다.");
+            assertThat(redisTemplate.keys(refreshToken).size()).isZero();
         }
 
     }

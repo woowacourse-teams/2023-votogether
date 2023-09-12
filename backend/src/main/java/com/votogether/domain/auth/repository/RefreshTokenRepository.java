c4ca281cd736837c27fec51aae9332f644b1b4ac
jeomxon <jeomxon@gmail.com>
2023-09-12 16:02:04 +0900

    refactor: (#532) 불필요한 클래스 및 인터페이스 제거

--- backend/src/main/java/com/votogether/domain/auth/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.votogether.domain.auth.repository;
-
-import com.votogether.domain.auth.RefreshToken;
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}

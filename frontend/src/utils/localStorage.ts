b05cf61826114c7926a0af2e3c766e90318f562e
Gilpop8663 <wolfye@naver.com>
2023-09-12 15:49:16 +0900

    refactor: (#508) 함수에 사용되는 매개변수 이름을 data에서 value로 변경

--- frontend/src/utils/localStorage.ts
+++ frontend/src/utils/localStorage.ts
@@ -2,8 +2,8 @@ export const getLocalStorage = <T>(key: string): T | null => {
   return JSON.parse(localStorage.getItem(key) || 'null');
 };
 
-export const setLocalStorage = <T>(key: string, data: T) => {
-  localStorage.setItem(key, JSON.stringify(data));
+export const setLocalStorage = <T>(key: string, value: T) => {
+  localStorage.setItem(key, JSON.stringify(value));
 };
 
 export const removeLocalStorage = (key: string) => {

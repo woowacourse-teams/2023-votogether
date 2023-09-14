918bc3dcaedf5aa69815008c2a02954548acdebe
Jun-Hyeok Sin <tjdtls690@gmail.com>
2023-09-14 17:34:17 +0900

    신고 도메인 리팩터링 (#540)
    * refactor: (#513) Report 도메인 코드 가독성 리팩토링
    
    * refactor: (#513) Report 도메인 관련 테스트 코드 리팩토링
    
    * refactor: (#513) Report의 Controller, Service를 컨벤션에 맞춰 클래스명 개선
    
    * refactor: (#513) 전략 패턴 클래스들에 대한 테스트 코드 추가
    
    * refactor: (#513) Transactional 어노테이션 클래스에 붙는 것으로 위치 개선
    
    * refactor: (#513) 닉네임 3회 신고 받은 후 해당 멤버에 대한 닉네임 신고 기록 삭제
    
    * refactor: (#513) 각 신고 전략에 관한 상수는 각 클래스에 두는 것으로 개선
    
    * refactor: (#513) 파라미터가 2개인 메서드의 선언문 개행 되돌리기
    
    * refactor: (#513) deleteByReportTypeAndTargetId 메서드의 테스트 코드 추가
    
    * refactor: (#513) 파라미터 2개인 메서드 선언부 개행 삭제

---
+++ backend/src/main/java/com/votogether/domain/report/service/strategy/ReportPostStrategy.java
@@ -0,0 +1,55 @@
+package com.votogether.domain.report.service.strategy;
+
+import com.votogether.domain.member.entity.Member;
+import com.votogether.domain.post.entity.Post;
+import com.votogether.domain.post.exception.PostExceptionType;
+import com.votogether.domain.post.repository.PostRepository;
+import com.votogether.domain.report.dto.request.ReportRequest;
+import com.votogether.domain.report.exception.ReportExceptionType;
+import com.votogether.domain.report.repository.ReportRepository;
+import com.votogether.global.exception.NotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
+public class ReportPostStrategy implements ReportStrategy {
+
+    private static final int NUMBER_OF_POST_BLIND_BASED_REPORTS = 5;
+
+    private final PostRepository postRepository;
+    private final ReportRepository reportRepository;
+
+    @Override
+    public void report(final Member reporter, final ReportRequest request) {
+        final Post reportedPost = postRepository.findById(request.id())
+                .orElseThrow(() -> new NotFoundException(PostExceptionType.POST_NOT_FOUND));
+        validatePost(reporter, reportedPost, request);
+
+        saveReport(reporter, request, reportRepository);
+        blindPost(request, reportedPost);
+    }
+
+    private void validatePost(
+            final Member reporter,
+            final Post reportedPost,
+            final ReportRequest request
+    ) {
+        reportedPost.validateMine(reporter);
+        reportedPost.validateHidden();
+        validateDuplicatedReport(
+                reporter,
+                request,
+                ReportExceptionType.DUPLICATE_POST_REPORT,
+                reportRepository
+        );
+    }
+
+    private void blindPost(final ReportRequest request, final Post reportedPost) {
+        final int reportCount = reportRepository.countByReportTypeAndTargetId(request.type(), request.id());
+        if (reportCount >= NUMBER_OF_POST_BLIND_BASED_REPORTS) {
+            reportedPost.blind();
+        }
+    }
+
+}
--- backend/src/test/java/com/votogether/test/persister/PostTestPersister.java
+++ backend/src/test/java/com/votogether/test/persister/PostTestPersister.java
@@ -4,8 +4,10 @@ import com.votogether.domain.member.entity.Member;
 import com.votogether.domain.post.entity.Post;
 import com.votogether.domain.post.entity.PostBody;
 import com.votogether.domain.post.repository.PostRepository;
+import com.votogether.domain.post.service.PostService;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 @RequiredArgsConstructor
 @Persister
@@ -23,6 +25,7 @@ public class PostTestPersister {
         private Member writer;
         private PostBody postBody;
         private LocalDateTime deadline;
+        private boolean isHidden;
 
         public PostBuilder writer(Member writer) {
             this.writer = writer;
@@ -39,12 +42,21 @@ public class PostTestPersister {
             return this;
         }
 
+        public PostBuilder blind() {
+            this.isHidden = true;
+            return this;
+        }
+
         public Post save() {
             Post post = Post.builder()
                     .writer(writer == null ? memberTestPersister.builder().save() : writer)
                     .postBody(postBody == null ? generatePostBody() : postBody)
                     .deadline(deadline == null ? LocalDateTime.of(2100, 12, 25, 0, 0) : deadline)
                     .build();
+            if (isHidden) {
+                post.blind();
+            }
+
             return postRepository.save(post);
         }
 
@@ -54,7 +66,6 @@ public class PostTestPersister {
                     .content("content")
                     .build();
         }
-
     }
 
 }

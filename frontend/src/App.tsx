ddc6b8b0e61aec996211643d78fbc1eb932c4e53
chsua <csj1919@naver.com>
2023-09-15 01:39:00 +0900

    chore: (#619) 리액트 쿼리 캐싱 확인을 위한 세팅

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider } from 'react-router-dom';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
 
 import { AuthProvider } from '@hooks/context/auth';
@@ -35,6 +36,7 @@ const App = () => (
             </AuthProvider>
           </PostOptionProvider>
         </ThemeProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </ErrorBoundaryForTopClass>
     <GoogleTagManager gtmId={process.env.VOTOGETHER_GOOGLE_TAG_ID} />

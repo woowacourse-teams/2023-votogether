048b1c98b2613e9107641556bf111c963a5b13f2
inyeong-kang <tomatorkd@naver.com>
2023-10-04 20:25:51 +0900

    refactor: (#687) 댓글 및 게시글 action 관련 상수 및 타입을 constants 및 types 폴더로 분리

--- frontend/src/types/menu.ts
+++ frontend/src/types/menu.ts
@@ -1,3 +1,5 @@
+import { COMMENT_ACTION, COMMENT_USER } from '@constants/post';
+
 export type MenuColor = 'black' | 'red';
 
 //메뉴 컴포넌트에서 사용하는 기본적인 인터페이스
@@ -7,9 +9,21 @@ interface MenuItem {
   action: string;
 }
 
-export type PostAction = 'NICKNAME_REPORT' | 'POST_REPORT' | 'DELETE';
+export type CommentAction = (typeof COMMENT_ACTION)[keyof typeof COMMENT_ACTION];
+
+export type PostAction = 'NICKNAME_REPORT' | 'POST_REPORT' | 'DELETE' | CommentAction;
 
 //게시글 메뉴 컴포넌트에서 사용하는 확장된 인터페이스 (action을 제한)
 export interface PostMenuItem extends MenuItem {
   action: PostAction;
+}
+
+export type CommentUser = (typeof COMMENT_USER)[keyof typeof COMMENT_USER];
+
+export type CommentMenu = Exclude<CommentUser, 'GUEST'>;
+
+export interface CommentMenuItem {
+  content: string;
+  color: 'black' | 'red';
+  action: CommentAction;
 }

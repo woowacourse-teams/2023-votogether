6ba3e912cb86e766426dc95e7d9391fb86e0da12
chsua <csj1919@naver.com>
2023-10-05 20:29:15 +0900

    feat: (#554) 토스트 컴포넌트를 그리기 위해 html에 새로운 root 생성

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -22,8 +22,12 @@ if (process.env.VOTOGETHER_GOOGLE_ANALYTICS_ID) {
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+const toastRoot = ReactDOM.createRoot(document.getElementById('toast-root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+toastRoot.render(<React.StrictMode></React.StrictMode>);

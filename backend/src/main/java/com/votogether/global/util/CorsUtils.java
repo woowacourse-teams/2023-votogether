b0848453e180eed638c54c180f6a3329e976d266
woo-chang <clllickme@naver.com>
2023-10-05 12:46:14 +0900

    refactor: (#402) 유틸 클래스 생성자 접근 제어자 수정

--- backend/src/main/java/com/votogether/global/util/CorsUtils.java
+++ backend/src/main/java/com/votogether/global/util/CorsUtils.java
@@ -1,9 +1,12 @@
 package com.votogether.global.util;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpMethod;
 
-public class CorsUtils {
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CorsUtils {
 
     public static boolean isPreflightRequest(final HttpServletRequest request) {
         return HttpMethod.OPTIONS.matches(request.getMethod());

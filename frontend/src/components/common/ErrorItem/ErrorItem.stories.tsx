4860aec366fa46c1157bbfb1e01349d4be947d3b
chsua <csj1919@naver.com>
2023-10-08 01:12:02 +0900

    test: 에러아이템 프롭스 수정에 따른 테스트코드 수정

--- frontend/src/components/common/ErrorItem/ErrorItem.stories.tsx
+++ frontend/src/components/common/ErrorItem/ErrorItem.stories.tsx
@@ -4,11 +4,37 @@ import ErrorItem from '.';
 
 const meta: Meta<typeof ErrorItem> = {
   component: ErrorItem,
+  args: {
+    text: '에러메세지를 작성할 수 있습니다.',
+    haveIcon: true,
+    retryInteraction: true,
+    homeInteraction: true,
+  },
+  argTypes: {
+    haveIcon: {
+      control: {
+        type: 'radio',
+      },
+      options: [true, false],
+    },
+    retryInteraction: {
+      control: {
+        type: 'radio',
+      },
+      options: [true, false],
+    },
+    homeInteraction: {
+      control: {
+        type: 'radio',
+      },
+      options: [true, false],
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof ErrorItem>;
 
 export const Default: Story = {
-  render: () => <ErrorItem />,
+  render: args => <ErrorItem {...args} />,
 };

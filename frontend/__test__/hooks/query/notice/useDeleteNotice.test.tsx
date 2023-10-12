844a93734e1d55277c0c0338dd6576d317d7d1a8
Gilpop8663 <wolfye@naver.com>
2023-10-12 17:52:01 +0900

    chore: (#737) 중복된 코드 삭제로 인한 테스트 코드 변경

--- frontend/__test__/hooks/query/notice/useDeleteNotice.test.tsx
+++ frontend/__test__/hooks/query/notice/useDeleteNotice.test.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { renderHook, waitFor } from '@testing-library/react';
 
-import { useNoticeDelete } from '@hooks';
+import { useDeleteNotice } from '@hooks';
 
 import { MOCK_NOTICE_TEST } from '@mocks/notice';
 
@@ -13,9 +13,9 @@ const wrapper = ({ children }: { children: ReactNode }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 );
 
-describe('useNoticeDelete 훅이 공지 사항을 삭제하는 지 확인한다.', () => {
+describe('useDeleteNotice 훅이 공지 사항을 삭제하는 지 확인한다.', () => {
   test('공지 사항을 삭제한다.', async () => {
-    const { result } = renderHook(() => useNoticeDelete(), {
+    const { result } = renderHook(() => useDeleteNotice(), {
       wrapper,
     });
 

59cc7aea3d0b756ee880a7c19ce4b216ffe55aac
inyeong-kang <tomatorkd@naver.com>
2023-10-13 14:42:23 +0900

    feat: (#730) 신고 목록 조회 페이지 UI 구현

---
+++ frontend/src/pages/admin/PendingReportPage/index.tsx
@@ -0,0 +1,109 @@
+import { useNavigate, useParams } from 'react-router-dom';
+
+import { LoadingSpinner, Table } from 'votogether-design-system';
+
+import { ReportActionRequest } from '@type/report';
+
+import { usePendingReportActionList } from '@hooks/query/report/usePendingReportActionList';
+import { useReportAction } from '@hooks/query/report/useReportAction';
+
+import Layout from '@components/common/Layout';
+
+import { PATH } from '@constants/path';
+import { REPORT_ACTION_TYPE, REPORT_TYPE } from '@constants/report';
+
+import * as S from './stlye';
+
+export interface ReportDetail {
+  typeName: string;
+  target: string;
+  isEdit: boolean;
+}
+
+export default function PendingReportPage() {
+  const navigate = useNavigate();
+
+  const params = useParams() as { page: string };
+  const currentPageNumber = params.page ? Number(params.page) : 1;
+
+  const columnList = ['Id', '사유', '내용', '일시', '종류', '수정/삭제', '신고 해제'];
+  const { data, isLoading } = usePendingReportActionList(currentPageNumber - 1);
+  const { mutate: reportAction } = useReportAction();
+
+  const handleClickReportAction = (reportData: ReportActionRequest, reportDetail: ReportDetail) => {
+    const { typeName, target, isEdit } = reportDetail;
+    const editOrDelete = isEdit ? '수정' : '삭제';
+    const message = `'${typeName}: ${target}'을 ${
+      reportData.hasAction ? editOrDelete : '해제'
+    }하시겠습니까?`;
+
+    if (window.confirm(message)) reportAction(reportData);
+  };
+
+  const reportListWithAction = data
+    ? data.reportList.map(report => {
+        const reportData = { id: report.id, hasAction: true };
+        const reportDetail = {
+          typeName: report.typeName,
+          target: report.target,
+          isEdit: report.typeName === REPORT_TYPE.NICKNAME,
+        };
+        return {
+          ...report,
+          editOrDeleteAction: (
+            <S.ReportActionButton
+              $isEdit={report.typeName === REPORT_TYPE.NICKNAME}
+              onClick={() => handleClickReportAction(reportData, reportDetail)}
+            >
+              {REPORT_ACTION_TYPE[report.typeName]}
+            </S.ReportActionButton>
+          ),
+          deleteReport: (
+            <button
+              onClick={() =>
+                handleClickReportAction({ ...reportData, hasAction: false }, reportDetail)
+              }
+            >
+              해제
+            </button>
+          ),
+        };
+      })
+    : [];
+
+  return (
+    <Layout isSidebarVisible={false}>
+      <S.Wrapper>
+        <S.PageTitle>신고 조치 예정 목록</S.PageTitle>
+        {isLoading && <LoadingSpinner size="md" />}
+        {data && (
+          <>
+            <Table columns={columnList} rows={reportListWithAction} />
+            <S.PaginationContainer>
+              <S.MovePageButton
+                onClick={() => navigate(`${PATH.PENDING_REPORT}?page=${data.currentPageCount - 1}`)}
+                disabled={currentPageNumber === 1}
+              >
+                이전
+              </S.MovePageButton>
+              {new Array(data.totalPageCount).fill(0).map((_, index) => (
+                <S.PaginationButton
+                  to={`${PATH.PENDING_REPORT}?page=${currentPageNumber}`}
+                  $isSelected={index + 1 === currentPageNumber}
+                >
+                  {index + 1}
+                </S.PaginationButton>
+              ))}
+              <S.MovePageButton
+                onClick={() => navigate(`${PATH.PENDING_REPORT}?page=${data.currentPageCount + 1}`)}
+                disabled={currentPageNumber === data.totalPageCount}
+              >
+                다음
+              </S.MovePageButton>
+            </S.PaginationContainer>
+          </>
+        )}
+      </S.Wrapper>
+    </Layout>
+  );
+}
--- frontend/src/types/report.ts
+++ frontend/src/types/report.ts
@@ -1,4 +1,7 @@
 import { REPORT_MESSAGE } from '@constants/policyMessage';
+import { REPORT_TYPE } from '@constants/report';
+
+import { StringDate } from './time';
 
 export type ReportType = 'POST' | 'COMMENT' | 'NICKNAME';
 
@@ -16,4 +19,30 @@ export type ReportMessage = keyof typeof REPORT_MESSAGE;
 export interface ReportInfo {
   name: string;
   reportMessageList: { [key: string]: string };
+}
+
+export interface PendingReportActionListResponse {
+  totalPageCount: number;
+  currentPageCount: number;
+  reports: PendingReportActionResponse[];
+}
+export interface PendingReportActionResponse {
+  id: number;
+  type: keyof typeof REPORT_TYPE;
+  reasons: string[];
+  createdAt: StringDate;
+  target: string;
+}
+
+export type PendingReportAction = Omit<PendingReportActionResponse, 'type'> & {
+  typeName: (typeof REPORT_TYPE)[keyof typeof REPORT_TYPE];
+};
+
+export type PendingReportActionList = Omit<PendingReportActionListResponse, 'reports'> & {
+  reportList: PendingReportAction[];
+};
+
+export interface ReportActionRequest {
+  id: number;
+  hasAction: boolean;
 }

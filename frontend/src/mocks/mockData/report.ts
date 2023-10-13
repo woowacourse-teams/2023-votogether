ec537e70c1491089039692d7e8329f8596eaba88
inyeong-kang <tomatorkd@naver.com>
2023-10-13 14:45:27 +0900

    feat: (#730) 신고 목록 조회 및 신고 조치 API 모킹

---
+++ frontend/src/mocks/mockData/report.ts
@@ -0,0 +1,29 @@
+import { PendingReportActionListResponse } from '@type/report';
+
+export const MOCK_PENDING_REPORT_LIST: PendingReportActionListResponse = {
+  totalPageCount: 5,
+  currentPageCount: 0,
+  reports: [
+    {
+      id: 0,
+      type: 'POST',
+      reasons: ['SPAMMING'],
+      target: '보투게더는 바보라고 생각하는 게시글',
+      createdAt: '2023-10-12 12:20',
+    },
+    {
+      id: 1,
+      type: 'COMMENT',
+      reasons: ['SPAMMING'],
+      target: '보투게더는 바보라고 생각하는 댓글',
+      createdAt: '2023-10-12 12:21',
+    },
+    {
+      id: 3,
+      type: 'NICKNAME',
+      reasons: ['SPAMMING'],
+      target: '보투게더바보',
+      createdAt: '2023-10-12 12:22',
+    },
+  ],
+};

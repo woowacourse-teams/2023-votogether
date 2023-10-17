4000618b9b752283e0e54a1ba2b8342fbe36994d
gilpop8663 <wolfye@naver.com>
2023-10-18 00:06:56 +0900

    refactor :(#741) 공지사항 목록을 감싸는 부모를 div에서 main으로 변경

--- frontend/src/pages/notice/NoticeListPage/style.ts
+++ frontend/src/pages/notice/NoticeListPage/style.ts
@@ -1,6 +1,6 @@
 import { styled } from 'styled-components';
 
-export const Container = styled.div`
+export const Container = styled.main`
   display: flex;
   align-items: center;
   flex-direction: column;

d45c1fe3327cf67a117f25458f86da2b13ffbb2b
Gilpop8663 <wolfye@naver.com>
2023-10-17 14:46:12 +0900

    chore: (#760) 디자인 시스템 설치 및 프로바이더 설정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,6 +4,7 @@ import { RouterProvider } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ThemeProvider } from 'styled-components';
+import { VDSProvider } from 'votogether-design-system';
 
 import { AuthProvider } from '@hooks/context/auth';
 import PostOptionProvider from '@hooks/context/postOption';
@@ -29,14 +30,16 @@ const App = () => (
   <ErrorBoundaryForTopClass>
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <GlobalStyle />
-        <PostOptionProvider>
-          <AuthProvider>
-            <Suspense fallback={<Skeleton isLarge />}>
-              <RouterProvider router={router} />
-            </Suspense>
-          </AuthProvider>
-        </PostOptionProvider>
+        <VDSProvider>
+          <GlobalStyle />
+          <PostOptionProvider>
+            <AuthProvider>
+              <Suspense fallback={<Skeleton isLarge />}>
+                <RouterProvider router={router} />
+              </Suspense>
+            </AuthProvider>
+          </PostOptionProvider>
+        </VDSProvider>
       </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>

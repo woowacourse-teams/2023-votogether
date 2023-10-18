cb0d689f3dfd0ccd9c60b587a4c411288984894c
inyeong-kang <tomatorkd@naver.com>
2023-10-18 12:03:50 +0900

    refactor: (#730) onError 추가, isLoading 등 사용하지 않는 속성 제거

--- frontend/src/hooks/query/report/usePendingReportActionList.ts
+++ frontend/src/hooks/query/report/usePendingReportActionList.ts
@@ -7,13 +7,19 @@ import { getPendingReportActionList } from '@api/report';
 import { QUERY_KEY } from '@constants/queryKey';
 
 export const usePendingReportActionList = (page: number) => {
-  const { data, error, isLoading, isError } = useQuery<PendingReportActionList>(
+  const { data } = useQuery<PendingReportActionList>(
     [QUERY_KEY.REPORT],
     () => getPendingReportActionList(page),
     {
       suspense: true,
+      onSuccess: data => {
+        return data;
+      },
+      onError: () => {
+        console.error('신고 조치 예정 목록 조회를 실패했습니다.');
+      },
     }
   );
 
-  return { data, error, isLoading, isError };
+  return { data };
 };

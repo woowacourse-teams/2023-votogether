6f432d7667320c48a4ae9c7a444c4e7c1df6319a
woo-chang <clllickme@naver.com>
2023-10-18 18:22:45 +0900

    chore: (#766) 개행 컨벤션 수정

--- backend/src/main/java/com/votogether/domain/alarm/service/AlarmCommandService.java
+++ backend/src/main/java/com/votogether/domain/alarm/service/AlarmCommandService.java
@@ -15,7 +15,11 @@ public class AlarmCommandService {
 
     private final AlarmReadStrategyProvider alarmReadStrategyProvider;
 
-    public void readAlarm(final Long alarmId, final String type, final Member loginMember) {
+    public void readAlarm(
+            final Long alarmId,
+            final String type,
+            final Member loginMember
+    ) {
         final AlarmActionType alarmActionType = AlarmActionType.from(type);
         final AlarmReadStrategy alarmReadStrategy = alarmReadStrategyProvider.getStrategy(alarmActionType);
         alarmReadStrategy.read(alarmId, loginMember);

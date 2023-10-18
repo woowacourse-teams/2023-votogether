f110aba5137d49014a88ef53d1f195142c666393
jeomxon <jeomxon@gmail.com>
2023-10-18 10:40:14 +0900

    refactor: (#743) docs 예외사항 추가 및 검증 어노테이션 추가

--- backend/src/main/java/com/votogether/domain/alarm/controller/AlarmController.java
+++ backend/src/main/java/com/votogether/domain/alarm/controller/AlarmController.java
@@ -6,11 +6,13 @@ import jakarta.validation.constraints.PositiveOrZero;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Validated
 @RequiredArgsConstructor
 @RequestMapping("/alarms")
 @RestController
--- backend/src/main/java/com/votogether/domain/alarm/controller/AlarmControllerDocs.java
+++ backend/src/main/java/com/votogether/domain/alarm/controller/AlarmControllerDocs.java
@@ -1,14 +1,16 @@
 package com.votogether.domain.alarm.controller;
 
 import com.votogether.domain.alarm.dto.response.PostAlarmResponse;
+import com.votogether.global.exception.ExceptionResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.constraints.PositiveOrZero;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Tag(name = "알림", description = "알림 API")
 public interface AlarmControllerDocs {
@@ -18,10 +20,15 @@ public interface AlarmControllerDocs {
             @ApiResponse(
                     responseCode = "200",
                     description = "게시글 내역 알림 조회 성공"
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "0 이상의 정수가 아닌 페이지",
+                    content = @Content(schema = @Schema(implementation = ExceptionResponse.class))
             )
     })
     ResponseEntity<List<PostAlarmResponse>> getPostAlarm(
-            @RequestParam @PositiveOrZero(message = "페이지는 0이상 정수만 가능합니다.") final int page
+            @PositiveOrZero(message = "페이지는 0이상 정수만 가능합니다.") final int page
     );
 
 }

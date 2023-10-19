3f6deae5f81c6b4328aee09bda3cd1cbd8a51eae
inyeong-kang <tomatorkd@naver.com>
2023-10-19 13:40:36 +0900

    refactor: (#704) 관리자 신고 관련 API 실패 시 Toast 띄우기

--- frontend/src/hooks/query/report/usePendingReportActionList.ts
+++ frontend/src/hooks/query/report/usePendingReportActionList.ts
@@ -1,12 +1,18 @@
+import { useContext } from 'react';
+
 import { useQuery } from '@tanstack/react-query';
 
 import { PendingReportActionList } from '@type/report';
+
+import { ToastContext } from '@hooks/context/toast';
 
 import { getPendingReportActionList } from '@api/report';
 
 import { QUERY_KEY } from '@constants/queryKey';
 
 export const usePendingReportActionList = (page: number) => {
+  const { addMessage } = useContext(ToastContext);
+
   const { data } = useQuery<PendingReportActionList>(
     [QUERY_KEY.REPORT],
     () => getPendingReportActionList(page),
@@ -15,8 +21,10 @@ export const usePendingReportActionList = (page: number) => {
       onSuccess: data => {
         return data;
       },
-      onError: () => {
-        console.error('신고 조치 예정 목록 조회를 실패했습니다.');
+      onError: error => {
+        const message =
+          error instanceof Error ? error.message : '신고 조치 예정 목록 조회를 실패했습니다.';
+        addMessage(message);
       },
     }
   );
--- frontend/src/hooks/query/report/useReportAction.ts
+++ frontend/src/hooks/query/report/useReportAction.ts
@@ -1,6 +1,10 @@
+import { useContext } from 'react';
+
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 import { ReportActionRequest } from '@type/report';
+
+import { ToastContext } from '@hooks/context/toast';
 
 import { reportAction } from '@api/report';
 
@@ -9,14 +13,17 @@ import { QUERY_KEY } from '@constants/queryKey';
 export const useReportAction = () => {
   const queryClient = useQueryClient();
 
+  const { addMessage } = useContext(ToastContext);
+
   const { mutate, isLoading, isSuccess, isError, error } = useMutation({
     mutationFn: async (reportActionData: ReportActionRequest) =>
       await reportAction(reportActionData),
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: [QUERY_KEY.REPORT] });
     },
-    onError: () => {
-      window.console.error('신고 조치를 실패했습니다.');
+    onError: error => {
+      const message = error instanceof Error ? error.message : '신고 조치를 실패했습니다.';
+      addMessage(message);
     },
   });
 

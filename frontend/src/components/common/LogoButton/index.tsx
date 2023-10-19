440eb6ae58c8d010195614dcc1d35faf2dcbd4cc
chsua <csj1919@naver.com>
2023-10-19 17:24:11 +0900

    feat: 마스코트 종류 추가

--- frontend/src/components/common/LogoButton/index.tsx
+++ frontend/src/components/common/LogoButton/index.tsx
@@ -44,7 +44,7 @@ export default function LogoButton({ content, ...rest }: LogoButtonProps) {
     return (
       <S.Button content={content} aria-label={ariaLabelText} {...rest}>
         <img src={logo} alt="로고 아이콘" onClick={handleLogoClick} />
-        {count > 5 && <EventMascot />}
+        {count > 5 && <EventMascot type={'ma'} />}
         <img src={votogether} alt="보투게더 아이콘" />
       </S.Button>
     );
@@ -53,7 +53,7 @@ export default function LogoButton({ content, ...rest }: LogoButtonProps) {
   return (
     <S.Button content={content} aria-label={ariaLabelText} {...rest}>
       <img src={src} alt="보투게더 아이콘" onClick={handleLogoClick} />
-      {count > 5 && <EventMascot />}
+      {count > 5 && <EventMascot type={'ma'} />}
     </S.Button>
   );
 }
--- frontend/src/pages/EventMascot/index.tsx
+++ frontend/src/pages/EventMascot/index.tsx
@@ -1,11 +1,12 @@
 import { useState } from 'react';
 
+import mascotYo from '@assets/dangseon-yo.png';
 import mascot from '@assets/dangseon.png';
 
 import * as S from './style';
 
 //이벤트를 위한 코드
-export default function EventMascot() {
+export default function EventMascot({ type }: { type: 'ma' | 'mayo' }) {
   const [isVisible, setIsVisible] = useState(true);
 
   const handleClick = () => {
@@ -14,7 +15,7 @@ export default function EventMascot() {
 
   return (
     <S.Container onClick={handleClick} $isVisible={isVisible}>
-      <S.Image src={mascot} />
+      <S.Image src={type === 'ma' ? mascot : mascotYo} />
     </S.Container>
   );
 }

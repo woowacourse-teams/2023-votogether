b96c1ece3690b8863c30035fe01bd84eae4c2961
chsua <csj1919@naver.com>
2023-10-24 22:44:27 +0900

    feat: (#816) 이벤트 코드 삭제

--- frontend/src/components/common/UpButton/index.tsx
+++ frontend/src/components/common/UpButton/index.tsx
@@ -1,31 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import chevronUp from '@assets/chevron_up_primary.svg';
-import mascotYo from '@assets/dangseon-yo.png';
 
 import * as S from './style';
 
 interface UpButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 export default function UpButton({ ...rest }: UpButtonProps) {
-  const [count, setCount] = useState(0);
-
-  //이벤트를 위한 코드
-  const handleLogoClick = () => {
-    if (count > 5) return;
-
-    setCount(count + 1);
-  };
-  const handleClick = () => {
-    setCount(0);
-  };
-
   return (
-    <>
-      <S.Button {...rest}>
-        <img src={chevronUp} alt="페이지 최상단으로 스크롤 올리기" onClick={handleLogoClick} />
-      </S.Button>
-      <S.Image src={mascotYo} onClick={handleClick} $isMoving={count > 5} />
-    </>
+    <S.Button {...rest}>
+      <img src={chevronUp} alt="페이지 최상단으로 스크롤 올리기" />
+    </S.Button>
   );
 }

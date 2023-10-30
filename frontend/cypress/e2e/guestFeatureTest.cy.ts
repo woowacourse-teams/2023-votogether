828d9c17d2507c15fa84348e1379245212b845c4
chsua <csj1919@naver.com>
2023-10-30 13:27:20 +0900

    test: (#818) 테스트명 한글로 수정

--- frontend/cypress/e2e/guestFeatureTest.cy.ts
+++ frontend/cypress/e2e/guestFeatureTest.cy.ts
@@ -6,55 +6,42 @@ describe('guestFeatureTest', () => {
   });
 
   /* ==== Test Created with Cypress Studio ==== */
-  it('controlPostListFilteringOrSorting', function () {
-    /* ==== Generated with Cypress Studio ==== */
+  it('게시글 목록을 필터링/정렬 조건을 수정한다', function () {
     cy.visit('http://localhost:3000/');
     cy.get(':nth-child(1) > .sc-gVpkOZ > .sc-fXmShK').click();
     cy.get('.sc-gyMiQo > :nth-child(1)').click();
     cy.get(':nth-child(2) > .sc-gVpkOZ > .sc-fXmShK').click();
     cy.get('.sc-gyMiQo > :nth-child(1)').click();
     //list 확인 필요
-    /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('lookRankingAndGoPopularPost', function () {
-    /* ==== Generated with Cypress Studio ==== */
+  it('랭킹 페이지에 진입해 인기글 상세 페이지로 이동한다', function () {
     cy.visit('http://localhost:3000/');
     cy.get('.sc-fWKGHs > .gHxHbZ > img').click();
     cy.get('.cbweqh').click();
     cy.get(':nth-child(1) > :nth-child(3) > a').click();
-    /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('lookNoticeListAndNoticeDetail', function () {
+  it('공지사항 목록페이지로 진입해 상세 공지사항 페이지로 이동한다', function () {
     /* ==== Generated with Cypress Studio ==== */
     cy.visit('http://localhost:3000/');
     cy.get('.sc-hmrlCG > .sc-iJSKBe > .sc-giQjdv > span').click();
     //list 확인 필요
     cy.get(':nth-child(1) > .sc-fGwJSW > .sc-hmftdN').click();
-    /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('showToastForGuest', function () {
-    /* ==== Generated with Cypress Studio ==== */
+  it('게스트에게 보이는 토스트를 확인한다', function () {
     cy.visit('http://localhost:3000/');
     cy.get('.sc-fWKGHs > .sc-gSIKdN > .sc-iNyIUv > img').click();
     cy.get('.sc-bdDrbm').should('exist').should('contain', '로그인 후 이용');
-    /* ==== End Cypress Studio ==== */
-    /* ==== Generated with Cypress Studio ==== */
+
     cy.get(
       '.sc-hmrlCG > .sc-iJSKBe > .sc-bYgGll > .sc-dILiZe > .sc-fKwBvW > .sc-dYlqv > .sc-hCcNSO > :nth-child(1) > .sc-fFCYnF > img'
     ).click();
     cy.get('.sc-bdDrbm').should('exist').should('contain', '로그인 후 이용');
-    /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('scrollTopScreen', function () {
-    /* ==== Generated with Cypress Studio ==== */
+  it('스크롤버튼을 눌러 상단으로 이동한다', function () {
     cy.visit('http://localhost:3000/');
     cy.scrollTo('bottom');
     cy.scrollTo('bottom');
@@ -63,18 +50,15 @@ describe('guestFeatureTest', () => {
     cy.get('html, body').should($el => {
       expect($el.scrollTop()).to.equal(0);
     });
-    /* ==== End Cypress Studio ==== */
   });
 
-  /* ==== Test Created with Cypress Studio ==== */
-  it('searchAKeyword', function () {
-    /* ==== Generated with Cypress Studio ==== */
+  it('a 키워드를 검색한다', function () {
     cy.visit('http://localhost:3000/');
     cy.get('.sc-jePPIV').click();
     cy.get('.sc-eIYifI').clear();
     cy.get('.sc-eIYifI').type('a');
     cy.get('.sc-otYyL > img').click();
     //list
-    /* ==== End Cypress Studio ==== */
   });
+  /* ==== Test Created with Cypress Studio ==== */
 });

6a805ca240e187d3114346b2417bfdd37c2bbf45
inyeong-kang <tomatorkd@naver.com>
2023-11-01 17:24:40 +0900

    chore: 불필요한 코드 삭제

--- frontend/src/pages/admin/PendingReportPage/index.tsx
+++ frontend/src/pages/admin/PendingReportPage/index.tsx
@@ -5,17 +5,10 @@ import { Skeleton } from 'votogether-design-system';
 import Layout from '@components/common/Layout';
 import UpButton from '@components/common/UpButton';
 
-import { REPORT_ACTION_TYPE } from '@constants/report';
-
 import { smoothScrollToTop } from '@utils/scrollToTop';
 
 import PendingReportTableFetcher from './PendingReportTableFetcher';
 import * as S from './style';
-
-export interface ReportDetail {
-  typeName: keyof typeof REPORT_ACTION_TYPE;
-  target: string;
-}
 
 export default function PendingReportPage() {
   return (

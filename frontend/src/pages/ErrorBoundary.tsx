c5b32c3e5346e1066ee55821990f29eec3065c9c
chsua <csj1919@naver.com>
2023-11-18 02:16:14 +0900

    feat: 일반 에러바운더리에도 404인 경우 해당 정보가 없다는 에러아이템 렌더링 적용

--- frontend/src/pages/ErrorBoundary.tsx
+++ frontend/src/pages/ErrorBoundary.tsx
@@ -30,6 +30,19 @@ class ErrorBoundary extends Component<ErrorBoundaryProps, ErrorBoundaryState> {
       const errorItemProps = { ...this.props };
       delete errorItemProps.children;
 
+      const error: { status: number; message: string } = JSON.parse(this.state.errorMessage);
+
+      if (error.status === 404) {
+        errorItemProps.hasRetryInteraction = false;
+
+        return (
+          <ErrorItem
+            text="해당 페이지는 존재하지 않습니다. 올바른 페이지 경로인지 확인해주세요."
+            {...errorItemProps}
+          />
+        );
+      }
+
       return <ErrorItem {...errorItemProps} />;
     }
 
